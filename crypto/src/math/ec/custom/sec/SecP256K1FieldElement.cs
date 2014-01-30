085f2a1a0171e406fa81b8aa9de2ede773a79eed
Peter Dettman <peter.dettman@bouncycastle.org>
2014-01-30 15:18:39 +0700

    Formatting

--- crypto/src/math/ec/custom/sec/SecP256K1FieldElement.cs
+++ crypto/src/math/ec/custom/sec/SecP256K1FieldElement.cs
@@ -145,43 +145,33 @@ namespace Org.BouncyCastle.Math.EC.Custom.Sec
             uint[] x2 = Nat256.Create();
             SecP256K1Field.Square(x1, x2);
             SecP256K1Field.Multiply(x2, x1, x2);
-
             uint[] x3 = Nat256.Create();
             SecP256K1Field.Square(x2, x3);
             SecP256K1Field.Multiply(x3, x1, x3);
-
             uint[] x6 = Nat256.Create();
             SecP256K1Field.SquareN(x3, 3, x6);
             SecP256K1Field.Multiply(x6, x3, x6);
-
             uint[] x9 = x6;
             SecP256K1Field.SquareN(x6, 3, x9);
             SecP256K1Field.Multiply(x9, x3, x9);
-
             uint[] x11 = x9;
             SecP256K1Field.SquareN(x9, 2, x11);
             SecP256K1Field.Multiply(x11, x2, x11);
-
             uint[] x22 = Nat256.Create();
             SecP256K1Field.SquareN(x11, 11, x22);
             SecP256K1Field.Multiply(x22, x11, x22);
-
             uint[] x44 = x11;
             SecP256K1Field.SquareN(x22, 22, x44);
             SecP256K1Field.Multiply(x44, x22, x44);
-
             uint[] x88 = Nat256.Create();
             SecP256K1Field.SquareN(x44, 44, x88);
             SecP256K1Field.Multiply(x88, x44, x88);
-
             uint[] x176 = Nat256.Create();
             SecP256K1Field.SquareN(x88, 88, x176);
             SecP256K1Field.Multiply(x176, x88, x176);
-
             uint[] x220 = x88;
             SecP256K1Field.SquareN(x176, 44, x220);
             SecP256K1Field.Multiply(x220, x44, x220);
-
             uint[] x223 = x44;
             SecP256K1Field.SquareN(x220, 3, x223);
             SecP256K1Field.Multiply(x223, x3, x223);
