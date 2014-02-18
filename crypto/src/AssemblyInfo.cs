f5df0e6e0e69ca1e12a7ad135aee65fe934de64a
Peter Dettman <peter.dettman@bouncycastle.org>
2014-02-18 17:36:07 +0700

    Changes to build system in preparation for beta release

--- crypto/src/AssemblyInfo.cs
+++ crypto/src/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.7.*")]
+[assembly: AssemblyVersion("1.8.*")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the
