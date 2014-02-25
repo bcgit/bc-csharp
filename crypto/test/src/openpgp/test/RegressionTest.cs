bad19a6fb0a82ae963d86d25c59864cb5a845b65
Peter Dettman <peter.dettman@bouncycastle.org>
2014-02-25 17:23:08 +0700

    Add new test to regression tests

--- crypto/test/src/openpgp/test/RegressionTest.cs
+++ crypto/test/src/openpgp/test/RegressionTest.cs
@@ -7,7 +7,7 @@ namespace Org.BouncyCastle.Bcpg.OpenPgp.Tests
     public class RegressionTest
     {
         public static ITest[] tests =
-		{
+        {
             new PgpKeyRingTest(),
             new PgpRsaTest(),
             new PgpDsaTest(),
@@ -15,18 +15,19 @@ namespace Org.BouncyCastle.Bcpg.OpenPgp.Tests
             new PgpPbeTest(),
             new PgpMarkerTest(),
             new PgpPacketTest(),
-			new PgpArmoredTest(),
-			new PgpSignatureTest(),
-			new PgpClearSignedSignatureTest(),
-            new PgpCompressionTest()
+            new PgpArmoredTest(),
+            new PgpSignatureTest(),
+            new PgpClearSignedSignatureTest(),
+            new PgpCompressionTest(),
+            new PGPNoPrivateKeyTest(),
         };
 
-		public static void Main(
-			string[] args)
+        public static void Main(
+            string[] args)
         {
-			foreach (ITest test in tests)
-			{
-				ITestResult result = test.Perform();
+            foreach (ITest test in tests)
+            {
+                ITestResult result = test.Perform();
                 Console.WriteLine(result);
             }
         }
