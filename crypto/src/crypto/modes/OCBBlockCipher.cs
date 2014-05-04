0e3b671aaffef29e62e72a09df99bee1b317beda
Peter Hultqvist <phq@silentorbit.com>
2014-05-04 18:00:41 +0200

    Ignoring compiler warning about missing xml documentation. Corrected syntax for <a> tag in comment

--- crypto/src/crypto/modes/OCBBlockCipher.cs
+++ crypto/src/crypto/modes/OCBBlockCipher.cs
@@ -7,8 +7,8 @@ using Org.BouncyCastle.Utilities;
 namespace Org.BouncyCastle.Crypto.Modes
 {
     /**
-     * An implementation of the "work in progress" Internet-Draft <a
-     * href="http://tools.ietf.org/html/draft-irtf-cfrg-ocb-07">The OCB Authenticated-Encryption
+     * An implementation of the "work in progress" Internet-Draft
+     * <a href="http://tools.ietf.org/html/draft-irtf-cfrg-ocb-07">The OCB Authenticated-Encryption
      * Algorithm</a>, licensed per:
      * 
      * <blockquote><p><a href="http://www.cs.ucdavis.edu/~rogaway/ocb/license1.pdf">License for
