9cc0e3e10440ba44ee9e8df4c2fdf2c35f2a7660
Peter Dettman <peter.dettman@bouncycastle.org>
2014-08-28 11:15:37 +0700

    Constants and support functions for draft-ietf-tls-negotiated-ff-dhe-01

--- crypto/src/crypto/tls/ExtensionType.cs
+++ crypto/src/crypto/tls/ExtensionType.cs
@@ -61,6 +61,13 @@ namespace Org.BouncyCastle.Crypto.Tls
         public static readonly int extended_master_secret = 100;
 
         /*
+         * draft-ietf-tls-negotiated-ff-dhe-01
+         * 
+         * WARNING: Placeholder value; the real value is TBA
+         */
+        public static readonly int negotiated_ff_dhe_groups = 101;
+
+        /*
          * RFC 5746 3.2.
          */
         public const int renegotiation_info = 0xff01;
