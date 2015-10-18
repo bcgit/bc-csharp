0f9254b9238d5a36df1217bb354e9e76282067d7
Peter Dettman <peter.dettman@bouncycastle.org>
2015-10-18 20:53:15 +0700

    http://www.bouncycastle.org/jira/browse/BMA-98
    - Support v3 encryption
    - Port more rewrap tests from Java API

--- crypto/src/openpgp/PgpUtilities.cs
+++ crypto/src/openpgp/PgpUtilities.cs
@@ -488,5 +488,18 @@ namespace Org.BouncyCastle.Bcpg.OpenPgp
                 throw new PgpException("unknown wrap algorithm: " + encAlgorithm);
             }
         }
+
+        internal static byte[] GenerateIV(int length, SecureRandom random)
+        {
+            byte[] iv = new byte[length];
+            random.NextBytes(iv);
+            return iv;
+        }
+
+        internal static S2k GenerateS2k(HashAlgorithmTag hashAlgorithm, int s2kCount, SecureRandom random)
+        {
+            byte[] iv = GenerateIV(8, random);
+            return new S2k(hashAlgorithm, iv, s2kCount);
+        }
     }
 }
