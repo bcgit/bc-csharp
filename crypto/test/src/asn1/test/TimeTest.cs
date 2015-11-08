fa43cd3f5404ec4a85366a9364e390a68a112b78
Peter Dettman <peter.dettman@bouncycastle.org>
2015-11-08 10:18:21 +0700

    DateTime portability changes for tests under PORTABLE

--- crypto/test/src/asn1/test/TimeTest.cs
+++ crypto/test/src/asn1/test/TimeTest.cs
@@ -15,9 +15,9 @@ namespace Org.BouncyCastle.Asn1.Tests
 			DateTime now = DateTime.UtcNow;
 
 			// Time classes only have a resolution of seconds
-			now = new DateTime(now.Year, now.Month, now.Day, now.Hour, now.Minute, now.Second);
+			now = SimpleTest.MakeUtcDateTime(now.Year, now.Month, now.Day, now.Hour, now.Minute, now.Second);
 
-			Org.BouncyCastle.Asn1.Cms.Time cmsTime = new Org.BouncyCastle.Asn1.Cms.Time(now);
+            Org.BouncyCastle.Asn1.Cms.Time cmsTime = new Org.BouncyCastle.Asn1.Cms.Time(now);
 			Org.BouncyCastle.Asn1.X509.Time x509Time = new Org.BouncyCastle.Asn1.X509.Time(now);
 
 //			Assert.AreEqual(cmsTime.Date, x509Time.ToDateTime());
