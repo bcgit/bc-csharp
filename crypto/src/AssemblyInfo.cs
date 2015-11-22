e8030b40e256f3c7639bbdb4af7495965972ae12
Peter Dettman <peter.dettman@bouncycastle.org>
2015-11-22 18:22:06 +0700

    More release preparation

--- crypto/src/AssemblyInfo.cs
+++ crypto/src/AssemblyInfo.cs
@@ -33,7 +33,9 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.8.*")]
+[assembly: AssemblyVersion("1.8.0.0")]
+[assembly: AssemblyFileVersion("1.8.15326.1")]
+[assembly: AssemblyInformationalVersion("1.8.0")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the
