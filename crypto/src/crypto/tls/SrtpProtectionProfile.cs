c1cca343de7db38649a051e594cd34fcc7742d8b
Peter Dettman <peter.dettman@bouncycastle.org>
2016-01-18 15:00:51 +0700

    Add DTLS-SRTP protection profiles from RFC 7714

--- crypto/src/crypto/tls/SrtpProtectionProfile.cs
+++ crypto/src/crypto/tls/SrtpProtectionProfile.cs
@@ -11,5 +11,11 @@ namespace Org.BouncyCastle.Crypto.Tls
         public const int SRTP_AES128_CM_HMAC_SHA1_32 = 0x0002;
         public const int SRTP_NULL_HMAC_SHA1_80 = 0x0005;
         public const int SRTP_NULL_HMAC_SHA1_32 = 0x0006;
+
+        /*
+         * RFC 7714 14.2.
+         */
+        public const int SRTP_AEAD_AES_128_GCM = 0x0007;
+        public const int SRTP_AEAD_AES_256_GCM = 0x0008;
     }
 }
