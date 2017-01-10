4045482a34a992a7fdca4cd754e5cb7b180f2892
Oren Novotny <oren@novotny.org>
2017-01-10 11:31:21 -0500

    update to VS 2017 RC2

--- crypto/src/AssemblyInfo.cs
+++ crypto/src/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 //using System.Security.Permissions;
 
 #if PORTABLE
@@ -71,6 +72,8 @@ using System.Runtime.InteropServices;
 #if !PORTABLE
 [assembly: ComVisible(false)]
 #endif
+
+[assembly: InternalsVisibleTo("crypto.test, PublicKey=002400000480000094000000060200000024000052534131000400000100010083A6A1D0D41B8A0FD3061C8DD2BA14DA98F9BF53576AD386A4D021ABD235EE41BC5416683314816908765FAC4951301E159153CF02BF1B31BEC8A2CE6C0110C30CC7BEF54E514D530B703D37629078AB3ECCE1AFA5ED3F9D63F3B50398188A811ADA59827B9E1A4EEEB87D05E4AFE45BEFD69BF2CDFD37F38334B748C8CB7FBC")]
 
 // Start with no permissions
 //[assembly: PermissionSet(SecurityAction.RequestOptional, Unrestricted=false)]
