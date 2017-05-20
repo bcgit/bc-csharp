303bae2a6c487cdf6b4093d4a1c0641ada1bef0c
Oren Novotny <oren@novotny.org>
2017-05-20 15:20:24 -0400

    build scripts

--- scripts/Sign-Package.ps1
+++ scripts/Sign-Package.ps1
@@ -17,7 +17,7 @@ $nupgks = ls $currentDirectory\..\*.nupkg | Select -ExpandProperty FullName
 foreach ($nupkg in $nupgks){
 	Write-Host "Submitting $nupkg for signing"
 
-	dotnet $appPath 'zip' -c $appSettings -i $nupkg -s $env:SignClientSecret -n 'Portable.BouncyCastle' -d 'Portable.BouncyCastle' -u 'https://github.com/onovotny/bc-sharp' 
+	dotnet $appPath 'sign' -c $appSettings -i $nupkg -s $env:SignClientSecret -n 'Portable.BouncyCastle' -d 'Portable.BouncyCastle' -u 'https://github.com/onovotny/bc-sharp' 
 
 	Write-Host "Finished signing $nupkg"
 }
