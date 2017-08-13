8654fced623d72412db1dfd806e77ca639125af1
Peter Dettman <peter.dettman@bouncycastle.org>
2017-08-13 23:20:15 +0700

    Fix Dstu7624WrapEngine to respect input offsets and lengths

--- crypto/src/crypto/engines/Dstu7624WrapEngine.cs
+++ crypto/src/crypto/engines/Dstu7624WrapEngine.cs
@@ -1,263 +1,216 @@
 ﻿using System;
+using System.Collections;
 
 using Org.BouncyCastle.Crypto.Parameters;
-using System.Collections.Generic;
+using Org.BouncyCastle.Crypto.Utilities;
 using Org.BouncyCastle.Utilities;
 
 namespace Org.BouncyCastle.Crypto.Engines
 {
-     public class Dstu7624WrapEngine : IWrapper 
-     {
-          private const int BYTES_IN_INTEGER = 4;
+    public class Dstu7624WrapEngine
+        : IWrapper
+    {
+        private KeyParameter param;
+        private Dstu7624Engine engine;
+        private bool forWrapping;
+        private int blockSize;
 
-          private KeyParameter param;
-          private Dstu7624Engine engine;
-          private bool forWrapping;
-          private int blockSize;
+        public Dstu7624WrapEngine(int blockSizeBits)
+        {
+            engine = new Dstu7624Engine(blockSizeBits);
+            param = null;
 
-          private byte[] buffer;
-          private byte[] intArray;
+            blockSize = blockSizeBits / 8;
+        }
 
-          private readonly byte[] checkSumArray, zeroArray;
+        public string AlgorithmName
+        {
+            get { return "Dstu7624WrapEngine"; }
+        }
 
+        public void Init(bool forWrapping, ICipherParameters parameters)
+        {
+            this.forWrapping = forWrapping;
 
-          public Dstu7624WrapEngine(int blockSizeBits)
-          {
-               engine = new Dstu7624Engine(blockSizeBits);
-               param = null;
-           
-               blockSize = blockSizeBits / 8;
-               buffer = new byte[blockSize];
-               
-               intArray = new byte[BYTES_IN_INTEGER];
+            if (parameters is KeyParameter)
+            {
+                this.param = (KeyParameter)parameters;
 
-               checkSumArray = new byte[blockSize];
-               zeroArray = new byte[blockSize];
-          }
-          
+                engine.Init(forWrapping, param);
+            }
+            else
+            {
+                throw new ArgumentException("Bad parameters passed to Dstu7624WrapEngine");
+            }
+        }
 
-          public string AlgorithmName
-          {
-               get { return "Dstu7624WrapEngine"; }
-          }
+        public byte[] Wrap(byte[] input, int inOff, int length)
+        {
+            if (!forWrapping)
+                throw new InvalidOperationException("Not set for wrapping");
 
-          public void Init(bool forWrapping, ICipherParameters parameters)
-          {
-               this.forWrapping = forWrapping;
-                            
-               if (parameters is KeyParameter)
-               {
-                    this.param = (KeyParameter)parameters;
+            if (length % blockSize != 0)
+                throw new ArgumentException("Padding not supported");
 
-                    engine.Init(forWrapping, param);
-               }
-               else
-               {
-                    throw new ArgumentException("Bad parameters passed to Dstu7624WrapEngine");
-               }
-          }
+            int n = 2 * (1 + length / blockSize);
+            int V = (n - 1) * 6;
 
-          public byte[] Wrap(byte[] input, int inOff, int length)
-          {
-               if (!forWrapping)
-               {
-                    throw new InvalidOperationException("Not set for wrapping");
-               }
+            byte[] buffer = new byte[length + blockSize];
+            Array.Copy(input, inOff, buffer, 0, length);
+            //Console.WriteLine(Org.BouncyCastle.Utilities.Encoders.Hex.ToHexString(buffer));
 
-               if ((input.Length - inOff) % blockSize != 0)
-               {
-                    throw new ArgumentException("Padding not supported");
-               }
+            byte[] B = new byte[blockSize / 2];
+            Array.Copy(buffer, 0, B, 0, blockSize / 2);
+            //Console.WriteLine("B0: "+ Org.BouncyCastle.Utilities.Encoders.Hex.ToHexString(B));
 
-               int n = 2 * (1 + input.Length / blockSize);
-               
-               int V = (n - 1) * 6;
+            IList bTemp = Platform.CreateArrayList();
+            int bHalfBlocksLen = buffer.Length - blockSize / 2;
+            int bufOff = blockSize / 2;
+            while (bHalfBlocksLen != 0)
+            {
+                byte[] temp = new byte[blockSize / 2];
+                Array.Copy(buffer, bufOff, temp, 0, blockSize / 2);
+                //Console.WriteLine(Org.BouncyCastle.Utilities.Encoders.Hex.ToHexString(buffer));
+                //Console.WriteLine(buffer.Length);
+                //Console.WriteLine("b: " + Org.BouncyCastle.Utilities.Encoders.Hex.ToHexString(temp));
 
-               buffer = new byte[input.Length - inOff + blockSize];
-               Array.Copy(input, inOff, buffer, 0, input.Length - inOff);
-               //Console.WriteLine(Org.BouncyCastle.Utilities.Encoders.Hex.ToHexString(buffer));
+                bTemp.Add(temp);
 
-               
+                bHalfBlocksLen -= blockSize / 2;
+                bufOff += blockSize / 2;
+            }
 
-               byte[] B = new byte[blockSize / 2];
-               Array.Copy(buffer, 0, B, 0, blockSize / 2);
-               //Console.WriteLine("B0: "+ Org.BouncyCastle.Utilities.Encoders.Hex.ToHexString(B));
-               
-               
-               List<byte[]> bTemp = new List<byte[]>();
-              
-               int bHalfBlocksLen = buffer.Length - blockSize  / 2;
-               int bufOff = blockSize / 2;
-               while (bHalfBlocksLen != 0)
-               {                    
-                    byte[] temp = new byte[blockSize / 2];
-                    Array.Copy(buffer, bufOff, temp, 0, blockSize / 2);
-                    //Console.WriteLine(Org.BouncyCastle.Utilities.Encoders.Hex.ToHexString(buffer));
-                    //Console.WriteLine(buffer.Length);
-                    //Console.WriteLine("b: " + Org.BouncyCastle.Utilities.Encoders.Hex.ToHexString(temp));
-                    
-                    bTemp.Add(temp);
+            for (int j = 0; j < V; j++)
+            {
+                Array.Copy(B, 0, buffer, 0, blockSize / 2);
+                Array.Copy((byte[])bTemp[0], 0, buffer, blockSize / 2, blockSize / 2);
 
-                    bHalfBlocksLen -= blockSize / 2;
-                    bufOff += blockSize / 2;
-               }
+                engine.ProcessBlock(buffer, 0, buffer, 0);
 
-                                       
+                byte[] intArray = Pack.UInt32_To_LE((uint)(j + 1));
+                for (int byteNum = 0; byteNum < intArray.Length; byteNum++)
+                {
+                    buffer[byteNum + blockSize / 2] ^= intArray[byteNum];
+                }
 
-               for (int j = 0; j < V; j++)
-               {
-                    Array.Copy(B, 0, buffer, 0, blockSize / 2);
-                    Array.Copy(bTemp[0], 0, buffer, blockSize / 2, blockSize / 2);
+                Array.Copy(buffer, blockSize / 2, B, 0, blockSize / 2);
 
-                    engine.ProcessBlock(buffer, 0, buffer, 0);
-                                        
-                    intTobytes(j + 1, intArray, 0);
-                    for (int byteNum = 0; byteNum < BYTES_IN_INTEGER; byteNum++)
-                    {
-                         buffer[byteNum + blockSize / 2] ^= intArray[byteNum];
-                    }
+                for (int i = 2; i < n; i++)
+                {
+                    Array.Copy((byte[])bTemp[i - 1], 0, (byte[])bTemp[i - 2], 0, blockSize / 2);
+                }
 
-                    Array.Copy(buffer, blockSize / 2, B, 0, blockSize / 2);
-                    
-                    for (int i = 2; i < n; i++)
-                    {                        
-                         Array.Copy(bTemp[i - 1], 0, bTemp[i - 2], 0, blockSize / 2);
-                    }
+                Array.Copy(buffer, 0, (byte[])bTemp[n - 2], 0, blockSize / 2);
 
-                    Array.Copy(buffer, 0, bTemp[n - 2], 0, blockSize / 2);
+                //Console.WriteLine("B" + j.ToString() + ": " + Org.BouncyCastle.Utilities.Encoders.Hex.ToHexString(B));
+                //Console.WriteLine("b: " + Org.BouncyCastle.Utilities.Encoders.Hex.ToHexString(bTemp[0]));
+                //Console.WriteLine("b: " + Org.BouncyCastle.Utilities.Encoders.Hex.ToHexString(bTemp[1]));
+                //Console.WriteLine("b: " + Org.BouncyCastle.Utilities.Encoders.Hex.ToHexString(bTemp[2]));
 
-                    //Console.WriteLine("B" + j.ToString() + ": " + Org.BouncyCastle.Utilities.Encoders.Hex.ToHexString(B));
-                    //Console.WriteLine("b: " + Org.BouncyCastle.Utilities.Encoders.Hex.ToHexString(bTemp[0]));
-                    //Console.WriteLine("b: " + Org.BouncyCastle.Utilities.Encoders.Hex.ToHexString(bTemp[1]));
-                    //Console.WriteLine("b: " + Org.BouncyCastle.Utilities.Encoders.Hex.ToHexString(bTemp[2]));
+                //Console.WriteLine(Org.BouncyCastle.Utilities.Encoders.Hex.ToHexString(buffer));
+            }
 
-                    //Console.WriteLine(Org.BouncyCastle.Utilities.Encoders.Hex.ToHexString(buffer));
-               }
+            Array.Copy(B, 0, buffer, 0, blockSize / 2);
+            bufOff = blockSize / 2;
 
+            for (int i = 0; i < n - 1; i++)
+            {
+                Array.Copy((byte[])bTemp[i], 0, buffer, bufOff, blockSize / 2);
+                bufOff += blockSize / 2;
+            }
 
-               Array.Copy(B, 0, buffer, 0, blockSize / 2);
-               bufOff = blockSize / 2;
+            return buffer;
+        }
 
-               for (int i = 0; i < n - 1; i++)
-               {
-                    Array.Copy(bTemp[i], 0, buffer, bufOff, blockSize / 2);
-                    bufOff += blockSize / 2;
-               }
+        public byte[] Unwrap(byte[] input, int inOff, int length)
+        {
+            if (forWrapping)
+                throw new InvalidOperationException("not set for unwrapping");
 
-               return buffer;
-          }
+            if (length % blockSize != 0)
+                throw new ArgumentException("Padding not supported");
 
-          public byte[] Unwrap(byte[] input, int inOff, int length)
-          {
-               if (forWrapping)
-               {
-                    throw new InvalidOperationException("not set for unwrapping");
-               }
+            int n = 2 * length / blockSize;
+            int V = (n - 1) * 6;
 
-               if ((input.Length - inOff) % blockSize != 0)
-               {
-                    throw new ArgumentException("Padding not supported");
-               }
+            byte[] buffer = new byte[length];
+            Array.Copy(input, inOff, buffer, 0, length);
 
-               int n = 2 * input.Length / blockSize;
+            byte[] B = new byte[blockSize / 2];
+            Array.Copy(buffer, 0, B, 0, blockSize / 2);
+            //Console.WriteLine("B18: " + Org.BouncyCastle.Utilities.Encoders.Hex.ToHexString(B));
 
-               int V = (n - 1) * 6;
+            IList bTemp = Platform.CreateArrayList();
 
-               buffer = new byte[input.Length - inOff];
-               Array.Copy(input, inOff, buffer, 0, input.Length - inOff);
-                              
-               byte[] B = new byte[blockSize / 2];
-               Array.Copy(buffer, 0, B, 0, blockSize / 2);
-               //Console.WriteLine("B18: " + Org.BouncyCastle.Utilities.Encoders.Hex.ToHexString(B));
-               
-               List<byte[]> bTemp = new List<byte[]>();
+            int bHalfBlocksLen = buffer.Length - blockSize / 2;
+            int bufOff = blockSize / 2;
+            while (bHalfBlocksLen != 0)
+            {
+                byte[] temp = new byte[blockSize / 2];
+                Array.Copy(buffer, bufOff, temp, 0, blockSize / 2);
+                //Console.WriteLine(Org.BouncyCastle.Utilities.Encoders.Hex.ToHexString(buffer));
+                //Console.WriteLine(buffer.Length);
+                //Console.WriteLine("b: " + Org.BouncyCastle.Utilities.Encoders.Hex.ToHexString(temp));
 
-               int bHalfBlocksLen = buffer.Length - blockSize / 2;
-               int bufOff = blockSize / 2;
-               while (bHalfBlocksLen != 0)
-               {
-                    byte[] temp = new byte[blockSize / 2];
-                    Array.Copy(buffer, bufOff, temp, 0, blockSize / 2);
-                    //Console.WriteLine(Org.BouncyCastle.Utilities.Encoders.Hex.ToHexString(buffer));
-                    //Console.WriteLine(buffer.Length);
-                    //Console.WriteLine("b: " + Org.BouncyCastle.Utilities.Encoders.Hex.ToHexString(temp));
+                bTemp.Add(temp);
 
-                    bTemp.Add(temp);
+                bHalfBlocksLen -= blockSize / 2;
+                bufOff += blockSize / 2;
+            }
 
-                    bHalfBlocksLen -= blockSize / 2;
-                    bufOff += blockSize / 2;     
-               }
+            for (int j = 0; j < V; j++)
+            {
+                Array.Copy((byte[])bTemp[n - 2], 0, buffer, 0, blockSize / 2);
+                Array.Copy(B, 0, buffer, blockSize / 2, blockSize / 2);
 
+                byte[] intArray = Pack.UInt32_To_LE((uint)(V - j));
+                for (int byteNum = 0; byteNum < intArray.Length; byteNum++)
+                {
+                    buffer[byteNum + blockSize / 2] ^= intArray[byteNum];
+                }
 
-               for (int j = 0; j < V; j++)
-               {
-                    Array.Copy(bTemp[n - 2], 0, buffer, 0, blockSize / 2);
-                    Array.Copy(B, 0, buffer, blockSize / 2, blockSize / 2);
-                    intTobytes(V - j, intArray, 0);
-                    for (int byteNum = 0; byteNum < BYTES_IN_INTEGER; byteNum++)
-                    {
-                         buffer[byteNum + blockSize / 2] ^= intArray[byteNum];
-                    }
+                //Console.WriteLine(Org.BouncyCastle.Utilities.Encoders.Hex.ToHexString(buffer));
 
-                    //Console.WriteLine(Org.BouncyCastle.Utilities.Encoders.Hex.ToHexString(buffer));
+                engine.ProcessBlock(buffer, 0, buffer, 0);
 
-                    engine.ProcessBlock(buffer, 0, buffer, 0);
+                //Console.WriteLine(Org.BouncyCastle.Utilities.Encoders.Hex.ToHexString(buffer));
 
-                    //Console.WriteLine(Org.BouncyCastle.Utilities.Encoders.Hex.ToHexString(buffer));
-                                        
-                    Array.Copy(buffer, 0, B, 0, blockSize / 2);
+                Array.Copy(buffer, 0, B, 0, blockSize / 2);
 
-                    for (int i = 2; i < n; i++)
-                    {
-                         Array.Copy(bTemp[n - i - 1], 0, bTemp[n - i], 0, blockSize / 2);
-                    }
+                for (int i = 2; i < n; i++)
+                {
+                    Array.Copy((byte[])bTemp[n - i - 1], 0, (byte[])bTemp[n - i], 0, blockSize / 2);
+                }
 
-                    Array.Copy(buffer, blockSize / 2, bTemp[0], 0, blockSize / 2);
+                Array.Copy(buffer, blockSize / 2, (byte[])bTemp[0], 0, blockSize / 2);
 
-                    //Console.WriteLine("B" + (V - j - 1).ToString() + ": " + Org.BouncyCastle.Utilities.Encoders.Hex.ToHexString(B));
-                    //Console.WriteLine("b: " + Org.BouncyCastle.Utilities.Encoders.Hex.ToHexString(bTemp[0]));
-                    //Console.WriteLine("b: " + Org.BouncyCastle.Utilities.Encoders.Hex.ToHexString(bTemp[1]));
-                    //Console.WriteLine("b: " + Org.BouncyCastle.Utilities.Encoders.Hex.ToHexString(bTemp[2]));
+                //Console.WriteLine("B" + (V - j - 1).ToString() + ": " + Org.BouncyCastle.Utilities.Encoders.Hex.ToHexString(B));
+                //Console.WriteLine("b: " + Org.BouncyCastle.Utilities.Encoders.Hex.ToHexString(bTemp[0]));
+                //Console.WriteLine("b: " + Org.BouncyCastle.Utilities.Encoders.Hex.ToHexString(bTemp[1]));
+                //Console.WriteLine("b: " + Org.BouncyCastle.Utilities.Encoders.Hex.ToHexString(bTemp[2]));
 
-                    //Console.WriteLine(Org.BouncyCastle.Utilities.Encoders.Hex.ToHexString(buffer));
-               }
+                //Console.WriteLine(Org.BouncyCastle.Utilities.Encoders.Hex.ToHexString(buffer));
+            }
 
+            Array.Copy(B, 0, buffer, 0, blockSize / 2);
+            bufOff = blockSize / 2;
 
-               Array.Copy(B, 0, buffer, 0, blockSize / 2);
-               bufOff = blockSize / 2;
+            for (int i = 0; i < n - 1; i++)
+            {
+                Array.Copy((byte[])bTemp[i], 0, buffer, bufOff, blockSize / 2);
+                bufOff += blockSize / 2;
+            }
 
-               for (int i = 0; i < n - 1; i++)
-               {
-                    Array.Copy(bTemp[i], 0, buffer, bufOff, blockSize / 2);
-                    bufOff += blockSize / 2;
-               }
+            byte diff = 0;
+            for (int i = buffer.Length - blockSize; i < buffer.Length; ++i)
+            {
+                diff |= buffer[i];
+            }
 
-               
-               Array.Copy(buffer, buffer.Length - blockSize, checkSumArray, 0, blockSize);
+            if (diff != 0)
+                throw new InvalidCipherTextException("checksum failed");
 
-
-               if (!Arrays.AreEqual(checkSumArray, zeroArray))
-               {
-                    throw new InvalidCipherTextException("checksum failed");
-               }
-               else
-               {
-                    Array.Resize(ref buffer, buffer.Length - blockSize);
-               }
-
-               return buffer;
-          }
-
-          //int to array of bytes
-          private static void intTobytes(
-                    int num,
-                    byte[] outBytes,
-                    int outOff)
-          {
-               outBytes[outOff + 3] = (byte)(num >> 24);
-               outBytes[outOff + 2] = (byte)(num >> 16);
-               outBytes[outOff + 1] = (byte)(num >> 8);
-               outBytes[outOff] = (byte)num;
-          }
-     }
+            return Arrays.CopyOfRange(buffer, 0, buffer.Length - blockSize);
+        }
+    }
 }
