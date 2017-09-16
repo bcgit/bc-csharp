574dbc0875687377c607a92765463986caa3da7a
Peter Dettman <peter.dettman@bouncycastle.org>
2017-09-16 15:21:21 +0700

    Port of SM2KeyExchange from Java

--- crypto/test/src/crypto/test/RegressionTest.cs
+++ crypto/test/src/crypto/test/RegressionTest.cs
@@ -128,6 +128,7 @@ namespace Org.BouncyCastle.Crypto.Tests
             new X931SignerTest(),
             new KeccakDigestTest(),
             new ShakeDigestTest(),
+            new SM2KeyExchangeTest(),
             new SM2SignerTest(),
         };
 
