4d690ade45c74f14bc58eaa41b53e86cc9f3601c
Oren Novotny <oren@novotny.org>
2017-09-18 23:01:39 -0400

    Update Sign Client

--- scripts/Sign-Package.ps1
+++ scripts/Sign-Package.ps1
@@ -10,14 +10,14 @@ if([string]::IsNullOrEmpty($env:SignClientSecret)){
 
 $appSettings = "$currentDirectory\SignClient.json"
 
-$appPath = "$currentDirectory\..\packages\SignClient\tools\SignClient.dll"
+$appPath = "$currentDirectory\..\packages\SignClient\tools\netcoreapp2.0\SignClient.dll"
 
 $nupgks = ls $currentDirectory\..\*.nupkg | Select -ExpandProperty FullName
 
 foreach ($nupkg in $nupgks){
 	Write-Host "Submitting $nupkg for signing"
 
-	dotnet $appPath 'sign' -c $appSettings -i $nupkg -s $env:SignClientSecret -n 'Portable.BouncyCastle' -d 'Portable.BouncyCastle' -u 'https://github.com/onovotny/bc-sharp' 
+	dotnet $appPath 'sign' -c $appSettings -i $nupkg -r $env:SignClientUser -s $env:SignClientSecret -n 'Portable.BouncyCastle' -d 'Portable.BouncyCastle' -u 'https://github.com/onovotny/bc-sharp' 
 
 	Write-Host "Finished signing $nupkg"
 }
