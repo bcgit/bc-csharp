bb1ac12b59e0365e9d1b59b6c492fa435f432a19
Peter Dettman <peter.dettman@bouncycastle.org>
2017-10-17 21:18:11 +0700

    Fix test for .NET 1.1

--- crypto/test/src/crypto/test/RsaTest.cs
+++ crypto/test/src/crypto/test/RsaTest.cs
@@ -24,7 +24,7 @@ namespace Org.BouncyCastle.Crypto.Tests
          * Based on https://github.com/crocs-muni/roca/blob/master/java/BrokenKey.java
          * Credits: ported to Java by Martin Paljak
          */
-        internal static class BrokenKey_CVE_2017_15361
+        internal class BrokenKey_CVE_2017_15361
         {
             private static readonly int[] prims = new int[]{ 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61,
                 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167 };
