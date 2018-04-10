509934dc20235670484572796fc41693f9d1ec2f
Devvox93 <devvox93@outlook.com>
2018-04-10 12:59:04 +0200

    Add bool for using DER encoding, added conditional encoding/decoding and added Plain encode/decode methods

--- crypto/src/crypto/signers/DsaDigestSigner.cs
+++ crypto/src/crypto/signers/DsaDigestSigner.cs
@@ -1,29 +1,29 @@
 using System;
-using System.Collections;
 using System.IO;
-using System.Text;
 
 using Org.BouncyCastle.Asn1;
-using Org.BouncyCastle.Crypto.Signers;
 using Org.BouncyCastle.Crypto.Parameters;
 using Org.BouncyCastle.Math;
 using Org.BouncyCastle.Security;
 
 namespace Org.BouncyCastle.Crypto.Signers
 {
-	public class DsaDigestSigner
+    public class DsaDigestSigner
 		: ISigner
 	{
 		private readonly IDigest digest;
 		private readonly IDsa dsaSigner;
 		private bool forSigning;
+        private bool useDerEncoding;
 
 		public DsaDigestSigner(
 			IDsa	signer,
-			IDigest	digest)
+			IDigest	digest,
+            bool useDerEncoding = true)
 		{
 			this.digest = digest;
 			this.dsaSigner = signer;
+            this.useDerEncoding = useDerEncoding;
 		}
 
 		public virtual string AlgorithmName
@@ -93,7 +93,7 @@ namespace Org.BouncyCastle.Crypto.Signers
 
 			BigInteger[] sig = dsaSigner.GenerateSignature(hash);
 
-			return DerEncode(sig[0], sig[1]);
+			return EncodeSignature(sig[0], sig[1]);
 		}
 
 		/// <returns>true if the internal state represents the signature described in the passed in array.</returns>
@@ -108,7 +108,7 @@ namespace Org.BouncyCastle.Crypto.Signers
 
 			try
 			{
-				BigInteger[] sig = DerDecode(signature);
+				BigInteger[] sig = DecodeSignature(signature);
 				return dsaSigner.VerifySignature(hash, sig[0], sig[1]);
 			}
 			catch (IOException)
@@ -123,7 +123,33 @@ namespace Org.BouncyCastle.Crypto.Signers
 			digest.Reset();
 		}
 
-		private byte[] DerEncode(
+        private byte[] EncodeSignature(
+            BigInteger r,
+            BigInteger s)
+        {
+            if (useDerEncoding)
+            {
+                return DerEncode(r, s);
+            } else
+            {
+                return PlainEncode(r, s);
+            }
+
+        }
+
+        private BigInteger[] DecodeSignature(byte[] rawSignature)
+        {
+            if (useDerEncoding)
+            {
+                return DerDecode(rawSignature);
+            } else
+            {
+                return PlainDecode(rawSignature);
+            }
+            
+        }
+
+        private byte[] DerEncode(
 			BigInteger	r,
 			BigInteger	s)
 		{
@@ -141,5 +167,34 @@ namespace Org.BouncyCastle.Crypto.Signers
 				((DerInteger) s[1]).Value
 			};
 		}
+
+        private byte[] PlainEncode(
+            BigInteger r,
+            BigInteger s)
+        {
+            var rBytes = r.ToByteArray();
+            var sBytes = s.ToByteArray();
+
+            var encodedSignature = new byte[rBytes.Length + sBytes.Length];
+            Array.Copy(rBytes, encodedSignature, rBytes.Length);
+            Array.Copy(sBytes, 0, encodedSignature, rBytes.Length, sBytes.Length);
+
+            return encodedSignature;
+        }
+
+        private BigInteger[] PlainDecode(
+            byte[] rawSignature)
+        {
+            var partLength = rawSignature.Length / 2;
+
+            // Add extra 0 in front to avoid negative numbers
+            var R = new byte[1 + partLength];
+            var S = new byte[1 + partLength];
+
+            Array.Copy(rawSignature, 0, R, 1, partLength);
+            Array.Copy(rawSignature, partLength, S, 1, partLength);
+
+            return new BigInteger[] { new BigInteger(R), new BigInteger(S) };
+        }
 	}
 }
