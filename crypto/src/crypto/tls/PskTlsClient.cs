f9523961d11f7e99eac3c79ba7dbef82ae325619
Peter Dettman <peter.dettman@bouncycastle.org>
2018-08-07 13:39:53 +0700

    TLS: Disable DHE_* ciphersuites in default clients

--- crypto/src/crypto/tls/PskTlsClient.cs
+++ crypto/src/crypto/tls/PskTlsClient.cs
@@ -32,8 +32,6 @@ namespace Org.BouncyCastle.Crypto.Tls
             {
                 CipherSuite.TLS_ECDHE_PSK_WITH_AES_128_CBC_SHA256,
                 CipherSuite.TLS_ECDHE_PSK_WITH_AES_128_CBC_SHA,
-                CipherSuite.TLS_DHE_PSK_WITH_AES_128_CBC_SHA256,
-                CipherSuite.TLS_DHE_PSK_WITH_AES_128_CBC_SHA
             };
         }
 
