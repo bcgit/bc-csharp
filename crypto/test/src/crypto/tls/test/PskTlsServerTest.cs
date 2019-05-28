6d3a6530c547b1d60bb86bdde315d0bf6b346e35
Oren Novotny <oren@novotny.org>
2019-05-28 12:01:11 -0400

    Update deps

--- crypto/test/src/crypto/tls/test/PskTlsServerTest.cs
+++ crypto/test/src/crypto/tls/test/PskTlsServerTest.cs
@@ -19,7 +19,7 @@ namespace Org.BouncyCastle.Crypto.Tls.Tests
     {
         private static readonly SecureRandom secureRandom = new SecureRandom();
 
-        public static void Main(string[] args)
+        public static void MainOld(string[] args)
         {
             int port = 5556;
 
