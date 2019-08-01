3fd3578972d69cabec38a060f1193f8c3138cc36
Peter Dettman <peter.dettman@bouncycastle.org>
2019-08-01 20:24:04 +0700

    Refactor ToString()

--- crypto/src/util/collections/CollectionUtilities.cs
+++ crypto/src/util/collections/CollectionUtilities.cs
@@ -49,23 +49,18 @@ namespace Org.BouncyCastle.Utilities.Collections
 
         public static string ToString(IEnumerable c)
         {
-            StringBuilder sb = new StringBuilder("[");
-
             IEnumerator e = c.GetEnumerator();
+            if (!e.MoveNext())
+                return "[]";
 
-            if (e.MoveNext())
+            StringBuilder sb = new StringBuilder("[");
+            sb.Append(e.Current.ToString());
+            while (e.MoveNext())
             {
+                sb.Append(", ");
                 sb.Append(e.Current.ToString());
-
-                while (e.MoveNext())
-                {
-                    sb.Append(", ");
-                    sb.Append(e.Current.ToString());
-                }
             }
-
             sb.Append(']');
-
             return sb.ToString();
         }
     }
