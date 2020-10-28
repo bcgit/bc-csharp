6536ed9e332d54431d00913685c02a3cf6bb287f
mw <megan@cryptoworkshop.com>
2020-10-29 10:22:06 +1100

    Added KMAC Params and test

---
+++ crypto/src/asn1/nist/KMACwithSHAKE256_params.cs
@@ -0,0 +1,111 @@
+﻿using Org.BouncyCastle.Utilities;
+using System;
+
+namespace Org.BouncyCastle.Asn1.Nist
+{
+    /// <summary>
+    /// KMACwithSHAKE256-params ::= SEQUENCE {
+    ///     kMACOutputLength     INTEGER DEFAULT 512, -- Output length in bits
+    ///     customizationString  OCTET STRING DEFAULT ''H
+    /// } 
+    /// </summary>
+public class KMACwithSHAKE256_params : Asn1Encodable
+{
+
+    private static readonly byte[] EMPTY_STRING = new byte[0];
+    private static readonly int DEF_LENGTH = 512;
+
+    private readonly int outputLength;
+    private readonly byte[] customizationString;
+
+
+    public KMACwithSHAKE256_params(int outputLength)
+    {
+        this.outputLength = outputLength;
+        this.customizationString = EMPTY_STRING;
+    }
+
+    public KMACwithSHAKE256_params(int outputLength, byte[] customizationString)
+    {
+        this.outputLength = outputLength;
+        this.customizationString = Arrays.Clone(customizationString);
+    }
+
+
+    public static KMACwithSHAKE256_params getInstance(Object o)
+    {
+        if (o is KMACwithSHAKE256_params)
+        {
+            return (KMACwithSHAKE256_params)o;
+        }
+        else if (o != null)
+        {
+            return new KMACwithSHAKE256_params(Asn1Sequence.GetInstance(o));
+        }
+
+        return null;
+    }
+
+
+    private KMACwithSHAKE256_params(Asn1Sequence seq)
+    {
+        if (seq.Count > 2)
+        {
+            throw new InvalidOperationException("sequence size greater than 2");
+        }
+
+        if (seq.Count == 2)
+        {
+            this.outputLength = DerInteger.GetInstance(seq[0]).IntValueExact;
+            this.customizationString = Arrays.Clone(Asn1OctetString.GetInstance(seq[1]).GetOctets());
+        }
+        else if (seq.Count == 1)
+        {
+            if (seq[0] is DerInteger)
+            {
+                this.outputLength = DerInteger.GetInstance(seq[0]).IntValueExact;
+                this.customizationString = EMPTY_STRING;
+            }
+            else
+            {
+                this.outputLength = DEF_LENGTH;
+                this.customizationString = Arrays.Clone(Asn1OctetString.GetInstance(seq[0]).GetOctets());
+            }
+        }
+        else
+        {
+            this.outputLength = DEF_LENGTH;
+            this.customizationString = EMPTY_STRING;
+        }
+    }
+
+
+
+    public int OutputLength
+    {
+        get { return outputLength; }
+    }
+
+    public byte[] CustomizationString
+    {
+        get { return Arrays.Clone(customizationString); }
+    }
+
+
+    public override Asn1Object ToAsn1Object()
+    {
+        Asn1EncodableVector v = new Asn1EncodableVector();
+        if (outputLength != DEF_LENGTH)
+        {
+            v.Add(new DerInteger(outputLength));
+        }
+
+        if (customizationString.Length != 0)
+        {
+            v.Add(new DerOctetString(CustomizationString));
+        }
+
+        return new DerSequence(v);
+    }
+}
+}
---
+++ crypto/test/src/asn1/test/KMacParamsTest.cs
@@ -0,0 +1,84 @@
+﻿using NUnit.Framework;
+using Org.BouncyCastle.Asn1.Nist;
+using Org.BouncyCastle.Utilities;
+using Org.BouncyCastle.Utilities.Test;
+
+namespace Org.BouncyCastle.Asn1.Tests
+{
+    [TestFixture]
+    public class KMacParamsTest  :SimpleTest
+    {
+        public override string Name => "KMacParamsTest";
+
+      
+        public override void PerformTest()
+        {
+            Assert.IsTrue(Arrays.AreEqual(new KMACwithSHAKE128_params(256).GetEncoded(), new DerSequence().GetEncoded()));
+            Assert.IsTrue(Arrays.AreEqual(new KMACwithSHAKE256_params(512).GetEncoded(), new DerSequence().GetEncoded()));
+
+            Assert.IsTrue(Arrays.AreEqual(new KMACwithSHAKE128_params(512).GetEncoded(), new DerSequence(new DerInteger(512)).GetEncoded()));
+            Assert.IsTrue(Arrays.AreEqual(new KMACwithSHAKE256_params(256).GetEncoded(), new DerSequence(new DerInteger(256)).GetEncoded()));
+
+            Assert.IsTrue(Arrays.AreEqual(new KMACwithSHAKE128_params(512).GetEncoded(), KMACwithSHAKE128_params.getInstance(new DerSequence(new DerInteger(512))).GetEncoded()));
+            Assert.IsTrue(Arrays.AreEqual(new KMACwithSHAKE256_params(256).GetEncoded(), KMACwithSHAKE256_params.getInstance(new DerSequence(new DerInteger(256))).GetEncoded()));
+
+            byte[] customizationString = Strings.ToByteArray("hello, world!");
+
+            Assert.IsTrue(Arrays.AreEqual(new KMACwithSHAKE128_params(512, customizationString).GetEncoded(), new DerSequence(
+                new Asn1Encodable[] { new DerInteger(512), new DerOctetString(customizationString) }).GetEncoded()));
+            Assert.IsTrue(Arrays.AreEqual(new KMACwithSHAKE256_params(256, customizationString).GetEncoded(), new DerSequence(
+                new Asn1Encodable[] { new DerInteger(256), new DerOctetString(customizationString) }).GetEncoded()));
+
+            Assert.IsTrue(Arrays.AreEqual(new KMACwithSHAKE128_params(512, customizationString).GetEncoded(),
+                KMACwithSHAKE128_params.getInstance(
+                    new DerSequence(new Asn1Encodable[] { new DerInteger(512), new DerOctetString(customizationString) })).GetEncoded()));
+            Assert.IsTrue(Arrays.AreEqual(new KMACwithSHAKE256_params(256, customizationString).GetEncoded(),
+                KMACwithSHAKE256_params.getInstance(new DerSequence(
+                new Asn1Encodable[] { new DerInteger(256), new DerOctetString(customizationString) })).GetEncoded()));
+
+            Assert.IsTrue(Arrays.AreEqual(new KMACwithSHAKE128_params(256, customizationString).GetEncoded(), new DerSequence(
+                new Asn1Encodable[] { new DerOctetString(customizationString) }).GetEncoded()));
+            Assert.IsTrue(Arrays.AreEqual(new KMACwithSHAKE256_params(512, customizationString).GetEncoded(), new DerSequence(
+                new Asn1Encodable[] { new DerOctetString(customizationString) }).GetEncoded()));
+
+            Assert.IsTrue(Arrays.AreEqual(new KMACwithSHAKE128_params(256, customizationString).GetEncoded(),
+                KMACwithSHAKE128_params.getInstance(
+                    new DerSequence(new Asn1Encodable[] { new DerOctetString(customizationString) })).GetEncoded()));
+            Assert.IsTrue(Arrays.AreEqual(new KMACwithSHAKE256_params(512, customizationString).GetEncoded(),
+                KMACwithSHAKE256_params.getInstance(new DerSequence(
+                new Asn1Encodable[] { new DerOctetString(customizationString) })).GetEncoded()));
+
+            KMACwithSHAKE128_params p128 = new KMACwithSHAKE128_params(256, customizationString);
+            Assert.AreEqual(256, p128.OutputLength);
+            Assert.IsTrue(Arrays.AreEqual(customizationString, p128.CustomizationString));
+            Assert.IsTrue(p128 == KMACwithSHAKE128_params.getInstance(p128));
+
+            KMACwithSHAKE256_params p256 = new KMACwithSHAKE256_params(512, customizationString);
+            Assert.AreEqual(512, p256.OutputLength);
+            Assert.IsTrue(Arrays.AreEqual(customizationString, p256.CustomizationString));
+            Assert.IsTrue(p256 == KMACwithSHAKE256_params.getInstance(p256));
+
+            p128 = new KMACwithSHAKE128_params(512);
+            Assert.AreEqual(512, p128.OutputLength);
+            Assert.IsTrue(Arrays.AreEqual(new byte[0], p128.CustomizationString));
+
+            p256 = new KMACwithSHAKE256_params(256);
+            Assert.AreEqual(256, p256.OutputLength);
+            Assert.IsTrue(Arrays.AreEqual(new byte[0], p256.CustomizationString));
+        }
+
+        public static void Main(
+            string[] args)
+        {
+            RunTest(new KMacParamsTest());
+        }
+
+        [Test]
+        public void TestFunction()
+        {
+            string resultText = Perform().ToString();
+
+            Assert.AreEqual(resultText, Name + ": Okay", resultText);
+        }
+    }
+}
