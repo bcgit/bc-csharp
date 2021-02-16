cc7fbb6bbe56f107cd4c54bcc1691911a851cd1a
Peter Dettman <peter.dettman@bouncycastle.org>
2021-02-16 21:32:24 +0700

    Update versions and release notes for 1.8.10

--- crypto/src/AssemblyInfo.cs
+++ crypto/src/AssemblyInfo.cs
@@ -33,9 +33,9 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.8.9.0")]
-[assembly: AssemblyFileVersion("1.8.20343.1")]
-[assembly: AssemblyInformationalVersion("1.8.9")]
+[assembly: AssemblyVersion("1.8.10.0")]
+[assembly: AssemblyFileVersion("1.8.21047.1")]
+[assembly: AssemblyInformationalVersion("1.8.10")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the
