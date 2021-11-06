0661cf3bc3259e0f14fc70d660c44c47f0b03a83
Peter Dettman <peter.dettman@bouncycastle.org>
2021-11-06 19:07:02 +0700

    Add Asn1Utilities class

---
+++ crypto/src/asn1/Asn1Utilities.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Org.BouncyCastle.Asn1
+{
+    public abstract class Asn1Utilities
+    {
+        public static string GetTagText(Asn1TaggedObject taggedObject)
+        {
+            // TODO[asn1] Implement and use taggedObject.TagClass
+            return GetTagText(Asn1Tags.ContextSpecific, taggedObject.TagNo);
+        }
+
+        public static string GetTagText(int tagClass, int tagNo)
+        {
+            switch (tagClass)
+            {
+            case Asn1Tags.Application:
+                return "[APPLICATION " + tagNo + "]";
+            case Asn1Tags.ContextSpecific:
+                return "[CONTEXT " + tagNo + "]";
+            case Asn1Tags.Private:
+                return "[PRIVATE " + tagNo + "]";
+            default:
+                return "[UNIVERSAL " + tagNo + "]";
+            }
+        }
+    }
+}
