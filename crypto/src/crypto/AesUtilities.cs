e59428c5953a53616bf8aec8aeb6fa1d3d453109
Peter Dettman <peter.dettman@bouncycastle.org>
2022-08-17 15:16:22 +0700

    AES intrinsics work

--- crypto/src/crypto/AesUtilities.cs
+++ crypto/src/crypto/AesUtilities.cs
@@ -7,11 +7,17 @@ namespace Org.BouncyCastle.Crypto
         public static IBlockCipher CreateEngine()
         {
 #if NETCOREAPP3_0_OR_GREATER
-            if (AesX86Engine.IsSupported)
-                return new AesX86Engine();
+            if (AesEngine_X86.IsSupported)
+                return new AesEngine_X86();
 #endif
 
             return new AesEngine();
         }
+
+#if NETCOREAPP3_0_OR_GREATER
+        public static bool IsHardwareAccelerated => AesEngine_X86.IsSupported;
+#else
+        public static bool IsHardwareAccelerated => false;
+#endif
     }
 }
