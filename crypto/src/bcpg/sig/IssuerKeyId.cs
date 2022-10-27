0f5e5e54e55c23222f6f934f2c32f212f13a34ae
Peter Dettman <peter.dettman@bouncycastle.org>
2022-10-27 19:33:38 +0700

    Refactoring in Bcpg.Sig

--- crypto/src/bcpg/sig/IssuerKeyId.cs
+++ crypto/src/bcpg/sig/IssuerKeyId.cs
@@ -1,6 +1,4 @@
-using System;
-
-
+using Org.BouncyCastle.Crypto.Utilities;
 
 namespace Org.BouncyCastle.Bcpg.Sig
 {
@@ -10,21 +8,9 @@ namespace Org.BouncyCastle.Bcpg.Sig
     public class IssuerKeyId
         : SignatureSubpacket
     {
-        protected static byte[] KeyIdToBytes(
-            long    keyId)
+        protected static byte[] KeyIdToBytes(long keyId)
         {
-            byte[]    data = new byte[8];
-
-            data[0] = (byte)(keyId >> 56);
-            data[1] = (byte)(keyId >> 48);
-            data[2] = (byte)(keyId >> 40);
-            data[3] = (byte)(keyId >> 32);
-            data[4] = (byte)(keyId >> 24);
-            data[5] = (byte)(keyId >> 16);
-            data[6] = (byte)(keyId >> 8);
-            data[7] = (byte)keyId;
-
-            return data;
+            return Pack.UInt64_To_BE((ulong)keyId);
         }
 
         public IssuerKeyId(
@@ -42,21 +28,6 @@ namespace Org.BouncyCastle.Bcpg.Sig
         {
         }
 
-        public long KeyId
-        {
-			get
-			{
-				long keyId = ((long)(data[0] & 0xff) << 56)
-					| ((long)(data[1] & 0xff) << 48)
-					| ((long)(data[2] & 0xff) << 40)
-					| ((long)(data[3] & 0xff) << 32)
-					| ((long)(data[4] & 0xff) << 24)
-					| ((long)(data[5] & 0xff) << 16)
-					| ((long)(data[6] & 0xff) << 8)
-					| ((long)data[7] & 0xff);
-
-				return keyId;
-			}
-        }
+        public long KeyId => (long)Pack.BE_To_UInt64(data);
     }
 }
