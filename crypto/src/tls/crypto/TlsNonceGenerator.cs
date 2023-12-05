409887caa82f47f5b81aadcf4d289f9f06fd6fbf
Peter Dettman <peter.dettman@bouncycastle.org>
2023-12-05 11:53:51 +0700

    Add TODO[api]

--- crypto/src/tls/crypto/TlsNonceGenerator.cs
+++ crypto/src/tls/crypto/TlsNonceGenerator.cs
@@ -8,5 +8,12 @@ namespace Org.BouncyCastle.Tls.Crypto
         /// <param name="size">the length, in bytes, of the nonce to generate.</param>
         /// <returns>the nonce value.</returns>
         byte[] GenerateNonce(int size);
+
+        // TODO[api]
+#if false //NETCOREAPP2_1_OR_GREATER || NETSTANDARD2_1_OR_GREATER
+        /// <summary>Generate a nonce in a given span.</summary>
+        /// <param name="output">the span to generate the nonce into.</param>
+        void GenerateNonce(Span<byte> output);
+#endif
     }
 }
