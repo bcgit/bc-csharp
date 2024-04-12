a50f679a981b253f36af5414285081f2c8bf9db3
Peter Dettman <peter.dettman@bouncycastle.org>
2024-04-12 23:11:20 +0700

    Refactoring in Tls

--- crypto/src/tls/DtlsServerProtocol.cs
+++ crypto/src/tls/DtlsServerProtocol.cs
@@ -740,11 +740,10 @@ namespace Org.BouncyCastle.Tls
             if (null == sessionVersion || !sessionVersion.IsDtls)
                 return false;
 
-            bool isEms = sessionParameters.IsExtendedMasterSecret;
-            if (!TlsUtilities.IsExtendedMasterSecretOptional(sessionVersion))
+            if (!sessionParameters.IsExtendedMasterSecret &&
+                !TlsUtilities.IsExtendedMasterSecretOptional(sessionVersion))
             {
-                if (!isEms)
-                    return false;
+                return false;
             }
 
             TlsCrypto crypto = state.serverContext.Crypto;
