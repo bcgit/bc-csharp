e44ebc3ada821408d29c3bd52fd862db4067064d
Peter Dettman <peter.dettman@bouncycastle.org>
2024-05-13 01:38:11 +0700

    Refactor to use Enum.TryParse

--- crypto/src/util/Enums.cs
+++ crypto/src/util/Enums.cs
@@ -6,25 +6,6 @@ namespace Org.BouncyCastle.Utilities
 {
     internal static class Enums
     {
-        internal static TEnum GetEnumValue<TEnum>(string s)
-            where TEnum : struct, Enum
-        {
-            // We only want to parse single named constants
-            if (s.Length > 0 && char.IsLetter(s[0]) && s.IndexOf(',') < 0)
-            {
-                s = s.Replace('-', '_');
-                s = s.Replace('/', '_');
-
-#if NETCOREAPP2_0_OR_GREATER || NETSTANDARD2_1_OR_GREATER
-                return Enum.Parse<TEnum>(s, false);
-#else
-                return (TEnum)Enum.Parse(typeof(TEnum), s, false);
-#endif
-            }
-
-            throw new ArgumentException();
-        }
-
         internal static TEnum[] GetEnumValues<TEnum>()
             where TEnum : struct, Enum
         {
@@ -41,6 +22,22 @@ namespace Org.BouncyCastle.Utilities
             TEnum[] values = GetEnumValues<TEnum>();
             int pos = (int)(DateTimeUtilities.CurrentUnixMs() & int.MaxValue) % values.Length;
             return values[pos];
+        }
+
+        internal static bool TryGetEnumValue<TEnum>(string s, out TEnum result)
+            where TEnum : struct, Enum
+        {
+            // We only want to parse single named constants
+            if (s.Length > 0 && char.IsLetter(s[0]) && s.IndexOf(',') < 0)
+            {
+                s = s.Replace('-', '_');
+                s = s.Replace('/', '_');
+
+                return Enum.TryParse<TEnum>(s, out result);
+            }
+
+            result = default(TEnum);
+            return false;
         }
     }
 }
