e9b1183bfa4bcffa21e03ed70dca875e777c0d82
Bence Mali <bence.mali@tresorit.com>
2024-08-30 12:35:01 +0200

    hybrid signature generation/verification

--- crypto/src/crypto/generators/HybridSignatureGenerator.cs
+++ crypto/src/crypto/generators/HybridSignatureGenerator.cs
@@ -2,13 +2,19 @@
 using Org.BouncyCastle.Crypto;
 using Org.BouncyCastle.Crypto.Generators;
 using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Crypto.Signers;
+using Org.BouncyCastle.Math.EC;
 using Org.BouncyCastle.Pqc.Crypto;
 using Org.BouncyCastle.Pqc.Crypto.Crystals.Dilithium;
 using Org.BouncyCastle.Pqc.Crypto.SphincsPlus;
 using Org.BouncyCastle.Security;
+using Org.BouncyCastle.Tls.Crypto.Impl.BC;
+using Org.BouncyCastle.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -76,12 +82,135 @@ namespace Org.BouncyCastle.crypto.generators
 
         public static byte[] GenerateSignature(AsymmetricKeyParameter privKey, byte[] message)
         {
-            // FIXME(bence)
+            if (!(privKey is HybridKeyParameters))
+            {
+                throw new ArgumentException("Provided key parameter is not hybrid");
+            }
+            if (!privKey.IsPrivate)
+            {
+                throw new ArgumentException("Provided key is not a private key");
+            }
+
+            var hybridPrivKey = privKey as HybridKeyParameters;
+
+            byte[] classicalSignature = null;
+            if (hybridPrivKey.Classical is ECPrivateKeyParameters)
+            {
+                var signer = new ECDsaSigner();
+                signer.Init(true, hybridPrivKey.Classical);
+                Math.BigInteger[] signature = signer.GenerateSignature(message);
+                classicalSignature = Arrays.Concatenate(signature[0].ToByteArrayUnsigned(), signature[1].ToByteArrayUnsigned());
+            }
+            else if (hybridPrivKey.Classical is RsaKeyParameters)
+            {
+                // TODO
+                throw new NotImplementedException("Rsa hybrid signature generation not supported");
+            }
+
+            if (classicalSignature == null)
+            {
+                throw new Exception("Classical keytype not supported");
+            }
+
+            byte[] postQuantumSignature = null;
+            if (hybridPrivKey.PostQuantum is DilithiumKeyParameters)
+            {
+                var signer = new DilithiumSigner();
+                signer.Init(true, hybridPrivKey.PostQuantum);
+                postQuantumSignature = signer.GenerateSignature(message);
+            }
+            else if (hybridPrivKey.PostQuantum is SphincsPlusKeyParameters)
+            {
+                var signer = new SphincsPlusSigner();
+                signer.Init(true, hybridPrivKey.PostQuantum);
+                postQuantumSignature = signer.GenerateSignature(message);
+            }
+
+            if (postQuantumSignature == null)
+            {
+                throw new Exception("Post-quantum keytype not supported");
+            }
+
+            return Arrays.Concatenate(classicalSignature, postQuantumSignature);
         }
 
         public static bool VerifySignature(AsymmetricKeyParameter pubKey, byte[] message, byte[] signature)
         {
-            // FIXME(bence)
+            if (!(pubKey is HybridKeyParameters))
+            {
+                throw new ArgumentException("Provided key parameter is not hybrid");
+            }
+
+            var hybridPubKey = pubKey as HybridKeyParameters;
+
+            bool classicalVerified = false;
+            if (hybridPubKey.Classical is ECPublicKeyParameters)
+            {
+                var domainParameters = (hybridPubKey.Classical as ECPublicKeyParameters).Parameters;
+                var classicalSignatureLength = (domainParameters.N.BitLength / 8) * 2;
+
+                if (signature.Length <= classicalSignatureLength)
+                {
+                    throw new Exception("Wrong size signature");
+                }
+
+                var classicalSignatureBytes = signature.Take(classicalSignatureLength).ToArray();
+                var r = new Math.BigInteger(classicalSignatureBytes.Take(classicalSignatureLength / 2).ToArray());
+                var s = new Math.BigInteger(classicalSignatureBytes.Skip(classicalSignatureLength / 2).ToArray());
+
+                var verifier = new ECDsaSigner();
+                verifier.Init(false, hybridPubKey.Classical);
+                if(!verifier.VerifySignature(message, r, s))
+                {
+                    throw new VerificationException("Signature verification failed");
+                }
+
+                // take away classical signature
+                signature = signature.Skip(classicalSignatureLength).ToArray();
+
+                classicalVerified = true;
+            }
+            else if (hybridPubKey.Classical is RsaKeyParameters)
+            {
+                // TODO
+                throw new NotImplementedException("Rsa hybrid signature verification not supported");
+            }
+
+            if (!classicalVerified)
+            {
+                throw new Exception("Classical keytype not supported");
+            }
+
+            bool postQuantumVerified = false;
+            if (hybridPubKey.PostQuantum is DilithiumPublicKeyParameters)
+            {
+                var verifier = new DilithiumSigner();
+                verifier.Init(false, hybridPubKey.PostQuantum);
+                if (!verifier.VerifySignature(message, signature))
+                {
+                    throw new VerificationException("Signature verification failed");
+                }
+
+                postQuantumVerified = true;
+            }
+            else if (hybridPubKey.PostQuantum is SphincsPlusPublicKeyParameters)
+            {
+                var verifier = new SphincsPlusSigner();
+                verifier.Init(false, hybridPubKey.PostQuantum);
+                if (!verifier.VerifySignature(message, signature))
+                {
+                    throw new VerificationException("Signature verification failed");
+                }
+
+                postQuantumVerified = true;
+            }
+
+            if (!postQuantumVerified)
+            {
+                throw new Exception("Post-quantum keytype not supported");
+            }
+
+            return true;
         }
     }
 }
