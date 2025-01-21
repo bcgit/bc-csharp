003c59792bbd9145844653ed577d706be934f06b
Peter Dettman <peter.dettman@bouncycastle.org>
2025-01-21 20:51:12 +0700

    Refactoring in Crypto.Encodings

--- crypto/src/crypto/encodings/ISO9796d1Encoding.cs
+++ crypto/src/crypto/encodings/ISO9796d1Encoding.cs
@@ -1,7 +1,8 @@
 using System;
-using Org.BouncyCastle.Crypto;
+
 using Org.BouncyCastle.Crypto.Parameters;
 using Org.BouncyCastle.Math;
+using Org.BouncyCastle.Security;
 
 namespace Org.BouncyCastle.Crypto.Encodings
 {
@@ -15,15 +16,10 @@ namespace Org.BouncyCastle.Crypto.Encodings
 	public class ISO9796d1Encoding
 		: IAsymmetricBlockCipher
 	{
-		private static readonly BigInteger Sixteen = BigInteger.ValueOf(16);
-		private static readonly BigInteger Six = BigInteger.ValueOf(6);
+        private static readonly byte[] Shadows = { 14, 3, 5, 8, 9, 4, 2, 15, 0, 13, 11, 6, 7, 10, 12, 1 };
+        private static readonly byte[] Inverse = { 8, 15, 6, 1, 5, 2, 11, 12, 3, 4, 13, 10, 14, 9, 0, 7 };
 
-		private static readonly byte[] shadows = { 0xe, 0x3, 0x5, 0x8, 0x9, 0x4, 0x2, 0xf,
-			0x0, 0xd, 0xb, 0x6, 0x7, 0xa, 0xc, 0x1 };
-		private static readonly byte[] inverse = { 0x8, 0xf, 0x6, 0x1, 0x5, 0x2, 0xb, 0xc,
-			0x3, 0x4, 0xd, 0xa, 0xe, 0x9, 0x0, 0x7 };
-
-		private readonly IAsymmetricBlockCipher engine;
+		private readonly IAsymmetricBlockCipher m_cipher;
 		private bool forEncryption;
 		private int bitSize;
 		private int padBits = 0;
@@ -31,80 +27,71 @@ namespace Org.BouncyCastle.Crypto.Encodings
 
 		public ISO9796d1Encoding(IAsymmetricBlockCipher cipher)
 		{
-			this.engine = cipher;
+			m_cipher = cipher;
 		}
 
-		public string AlgorithmName => engine.AlgorithmName + "/ISO9796-1Padding";
+		public string AlgorithmName => m_cipher.AlgorithmName + "/ISO9796-1Padding";
 
-		public IAsymmetricBlockCipher UnderlyingCipher => engine;
+		public IAsymmetricBlockCipher UnderlyingCipher => m_cipher;
 
         public void Init(bool forEncryption, ICipherParameters parameters)
         {
-            RsaKeyParameters kParam;
-			if (parameters is ParametersWithRandom withRandom)
-			{
-				kParam = (RsaKeyParameters)withRandom.Parameters;
-			}
-			else
-			{
-				kParam = (RsaKeyParameters)parameters;
-			}
+            m_cipher.Init(forEncryption, parameters);
 
-			engine.Init(forEncryption, parameters);
+            parameters = ParameterUtilities.IgnoreRandom(parameters);
 
-			modulus = kParam.Modulus;
-			bitSize = modulus.BitLength;
+            RsaKeyParameters kParam = (RsaKeyParameters)parameters;
 
-			this.forEncryption = forEncryption;
-		}
+            this.modulus = kParam.Modulus;
+            this.bitSize = modulus.BitLength;
 
-		/**
+            this.forEncryption = forEncryption;
+        }
+
+        /**
 		* return the input block size. The largest message we can process
 		* is (key_size_in_bits + 3)/16, which in our world comes to
 		* key_size_in_bytes / 2.
 		*/
-		public int GetInputBlockSize()
-		{
-			int baseBlockSize = engine.GetInputBlockSize();
+        public int GetInputBlockSize()
+        {
+            int baseBlockSize = m_cipher.GetInputBlockSize();
 
-			if (forEncryption)
-			{
-				return (baseBlockSize + 1) / 2;
-			}
-			else
-			{
-				return baseBlockSize;
-			}
-		}
+            if (forEncryption)
+            {
+                return (baseBlockSize + 1) / 2;
+            }
+            else
+            {
+                return baseBlockSize;
+            }
+        }
 
-		/**
+        /**
 		* return the maximum possible size for the output.
 		*/
-		public int GetOutputBlockSize()
-		{
-			int baseBlockSize = engine.GetOutputBlockSize();
+        public int GetOutputBlockSize()
+        {
+            int baseBlockSize = m_cipher.GetOutputBlockSize();
 
-			if (forEncryption)
-			{
-				return baseBlockSize;
-			}
-			else
-			{
-				return (baseBlockSize + 1) / 2;
-			}
-		}
+            if (forEncryption)
+            {
+                return baseBlockSize;
+            }
+            else
+            {
+                return (baseBlockSize + 1) / 2;
+            }
+        }
 
-		/**
+        /**
 		* set the number of bits in the next message to be treated as
 		* pad bits.
 		*/
-		public void SetPadBits(
-			int     padBits)
+        public void SetPadBits(int padBits)
 		{
-			if (padBits > 7)
-			{
-				throw new ArgumentException("padBits > 7");
-			}
+			if ((uint)padBits > 7U)
+				throw new ArgumentOutOfRangeException(nameof(padBits));
 
 			this.padBits = padBits;
 		}
@@ -112,152 +99,126 @@ namespace Org.BouncyCastle.Crypto.Encodings
 		/**
 		* retrieve the number of pad bits in the last decoded message.
 		*/
-		public int GetPadBits()
-		{
-			return padBits;
-		}
+		public int GetPadBits() => padBits;
 
-		public byte[] ProcessBlock(
-			byte[]	input,
-			int		inOff,
-			int		length)
-		{
-			if (forEncryption)
-			{
-				return EncodeBlock(input, inOff, length);
-			}
-			else
-			{
-				return DecodeBlock(input, inOff, length);
-			}
-		}
+        public byte[] ProcessBlock(byte[] input, int inOff, int length)
+        {
+			return forEncryption
+				?	EncodeBlock(input, inOff, length)
+				:	DecodeBlock(input, inOff, length);
+        }
 
-		private byte[] EncodeBlock(
-			byte[]	input,
-			int		inOff,
-			int		inLen)
-		{
-			byte[]  block = new byte[(bitSize + 7) / 8];
-			int     r = padBits + 1;
-			int     z = inLen;
-			int     t = (bitSize + 13) / 16;
+        private byte[] EncodeBlock(byte[] input, int inOff, int inLen)
+        {
+            byte[] block = new byte[(bitSize + 7) / 8];
+            int r = padBits + 1;
+            int z = inLen;
+            int t = (bitSize + 13) / 16;
 
-			for (int i = 0; i < t; i += z)
-			{
-				if (i > t - z)
-				{
-					Array.Copy(input, inOff + inLen - (t - i),
-						block, block.Length - t, t - i);
-				}
-				else
-				{
-					Array.Copy(input, inOff, block, block.Length - (i + z), z);
-				}
-			}
+            for (int i = 0; i < t; i += z)
+            {
+                if (i > t - z)
+                {
+                    Array.Copy(input, inOff + z - (t - i), block, block.Length - t, t - i);
+                }
+                else
+                {
+                    Array.Copy(input, inOff, block, block.Length - (i + z), z);
+                }
+            }
 
-			for (int i = block.Length - 2 * t; i != block.Length; i += 2)
-			{
-				byte val = block[block.Length - t + i / 2];
+            for (int i = block.Length - 2 * t; i != block.Length; i += 2)
+            {
+                byte val = block[block.Length - t + i / 2];
 
-				block[i] = (byte)((shadows[(uint) (val & 0xff) >> 4] << 4)
-					| shadows[val & 0x0f]);
-				block[i + 1] = val;
-			}
+                block[i] = (byte)(Shadows[val >> 4] << 4 | Shadows[val & 0x0F]);
+                block[i + 1] = val;
+            }
 
-			block[block.Length - 2 * z] ^= (byte) r;
-			block[block.Length - 1] = (byte)((block[block.Length - 1] << 4) | 0x06);
+            block[block.Length - 2 * z] ^= (byte)r;
+            block[block.Length - 1] = (byte)((block[block.Length - 1] << 4) | 0x06);
 
-			int maxBit = (8 - (bitSize - 1) % 8);
-			int offSet = 0;
+            int maxBit = 8 - (bitSize - 1) % 8;
+            int offset = 0;
 
-			if (maxBit != 8)
-			{
-				block[0] &= (byte) ((ushort) 0xff >> maxBit);
-				block[0] |= (byte) ((ushort) 0x80 >> maxBit);
-			}
-			else
-			{
-				block[0] = 0x00;
-				block[1] |= 0x80;
-				offSet = 1;
-			}
+            if (maxBit != 8)
+            {
+                block[0] &= (byte)(0xFFU >> maxBit);
+                block[0] |= (byte)(0x80U >> maxBit);
+            }
+            else
+            {
+                block[0] = 0x00;
+                block[1] |= 0x80;
+                offset = 1;
+            }
 
-			return engine.ProcessBlock(block, offSet, block.Length - offSet);
-		}
+            return m_cipher.ProcessBlock(block, offset, block.Length - offset);
+        }
 
-		/**
+        /**
 		* @exception InvalidCipherTextException if the decrypted block is not a valid ISO 9796 bit string
 		*/
-		private byte[] DecodeBlock(
-			byte[]	input,
-			int		inOff,
-			int		inLen)
-		{
-			byte[]  block = engine.ProcessBlock(input, inOff, inLen);
-			int     r = 1;
-			int     t = (bitSize + 13) / 16;
+        private byte[] DecodeBlock(byte[] input, int inOff, int inLen)
+        {
+            byte[] block = m_cipher.ProcessBlock(input, inOff, inLen);
+            int r = 1;
+            int t = (bitSize + 13) / 16;
 
-			BigInteger iS = new BigInteger(1, block);
-			BigInteger iR;
-			if (iS.Mod(Sixteen).Equals(Six))
-			{
-				iR = iS;
-			}
-			else
-			{
-				iR = modulus.Subtract(iS);
+            BigInteger iS = new BigInteger(1, block);
+            BigInteger iR;
+            if ((iS.IntValue & 15) == 6)
+            {
+                iR = iS;
+            }
+            else
+            {
+                iR = modulus.Subtract(iS);
 
-				if (!iR.Mod(Sixteen).Equals(Six))
-					throw new InvalidCipherTextException("resulting integer iS or (modulus - iS) is not congruent to 6 mod 16");
-			}
+                if ((iR.IntValue & 15) != 6)
+                    throw new InvalidCipherTextException("resulting integer iS or (modulus - iS) is not congruent to 6 mod 16");
+            }
 
-			block = iR.ToByteArrayUnsigned();
+            block = iR.ToByteArrayUnsigned();
 
-			if ((block[block.Length - 1] & 0x0f) != 0x6)
-				throw new InvalidCipherTextException("invalid forcing byte in block");
+            if ((block[block.Length - 1] & 0xF) != 0x6)
+                throw new InvalidCipherTextException("invalid forcing byte in block");
 
-			block[block.Length - 1] =
-				(byte)(((ushort)(block[block.Length - 1] & 0xff) >> 4)
-				| ((inverse[(block[block.Length - 2] & 0xff) >> 4]) << 4));
+            block[block.Length - 1] = (byte)(block[block.Length - 1] >> 4 | Inverse[block[block.Length - 2] >> 4] << 4);
 
-			block[0] = (byte)((shadows[(uint) (block[1] & 0xff) >> 4] << 4)
-				| shadows[block[1] & 0x0f]);
+            block[0] = (byte)(Shadows[block[1] >> 4] << 4 | Shadows[block[1] & 0x0F]);
 
-			bool boundaryFound = false;
-			int boundary = 0;
+            bool boundaryFound = false;
+            int boundary = 0;
 
-			for (int i = block.Length - 1; i >= block.Length - 2 * t; i -= 2)
-			{
-				int val = ((shadows[(uint) (block[i] & 0xff) >> 4] << 4)
-					| shadows[block[i] & 0x0f]);
+            for (int i = block.Length - 1; i >= block.Length - 2 * t; i -= 2)
+            {
+                int val = Shadows[block[i] >> 4] << 4 | Shadows[block[i] & 0x0F];
 
-				if (((block[i - 1] ^ val) & 0xff) != 0)
-				{
-					if (!boundaryFound)
-					{
-						boundaryFound = true;
-						r = (block[i - 1] ^ val) & 0xff;
-						boundary = i - 1;
-					}
-					else
-					{
-						throw new InvalidCipherTextException("invalid tsums in block");
-					}
-				}
-			}
+                int x = val ^ block[i - 1];
+                if (x != 0)
+                {
+                    if (boundaryFound)
+                        throw new InvalidCipherTextException("invalid tsums in block");
 
-			block[boundary] = 0;
+                    boundaryFound = true;
+                    r = x;
+                    boundary = i - 1;
+                }
+            }
 
-			byte[] nblock = new byte[(block.Length - boundary) / 2];
+            block[boundary] = 0;
 
-			for (int i = 0; i < nblock.Length; i++)
-			{
-				nblock[i] = block[2 * i + boundary + 1];
-			}
+            byte[] nblock = new byte[(block.Length - boundary) / 2];
 
-			padBits = r - 1;
+            for (int i = 0; i < nblock.Length; i++)
+            {
+                nblock[i] = block[2 * i + boundary + 1];
+            }
 
-			return nblock;
-		}
-	}
+            padBits = r - 1;
+
+            return nblock;
+        }
+    }
 }
