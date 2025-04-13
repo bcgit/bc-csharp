bc11c17b1cf28205e2248ae08f8599b4ea5a8038
Peter Dettman <peter.dettman@bouncycastle.org>
2025-04-13 21:19:26 +0700

    Update IssuingDistributionPoint from bc-java

--- crypto/test/src/asn1/test/IssuingDistributionPointTest.cs
+++ crypto/test/src/asn1/test/IssuingDistributionPointTest.cs
@@ -2,119 +2,86 @@ using System;
 
 using NUnit.Framework;
 
-using Org.BouncyCastle.Asn1;
 using Org.BouncyCastle.Asn1.X509;
 using Org.BouncyCastle.Utilities.Test;
 
 namespace Org.BouncyCastle.Asn1.Tests
 {
-	[TestFixture]
-	public class IssuingDistributionPointUnitTest 
-		: SimpleTest
-	{
-		public override string Name
-		{
-			get { return "IssuingDistributionPoint"; }
-		}
+    [TestFixture]
+    public class IssuingDistributionPointUnitTest
+        : SimpleTest
+    {
+        public override string Name => "IssuingDistributionPoint";
 
-		public override void PerformTest()
-		{
-			DistributionPointName name = new DistributionPointName(
-				new GeneralNames(new GeneralName(new X509Name("cn=test"))));
-			ReasonFlags reasonFlags = new ReasonFlags(ReasonFlags.CACompromise);
+        public override void PerformTest()
+        {
+            DistributionPointName name = new DistributionPointName(
+                new GeneralNames(new GeneralName(new X509Name("cn=test"))));
+            ReasonFlags reasonFlags = new ReasonFlags(ReasonFlags.CACompromise);
 
-			checkPoint(6, name, true, true, reasonFlags, true, true);
+            CheckOnlyException(name, true, true, reasonFlags, true, true);
+            CheckOnlyException(name, true, true, reasonFlags, true, false);
+            CheckOnlyException(name, true, false, reasonFlags, true, true);
 
-			checkPoint(2, name, false, false, reasonFlags, false, false);
+            CheckPoint(2, name, false, false, reasonFlags, false, false);
+            CheckPoint(0, null, false, false, null, false, false);
 
-			checkPoint(0, null, false, false, null, false, false);
+            try
+            {
+                IssuingDistributionPoint.GetInstance(new object());
 
-			try
-			{
-				IssuingDistributionPoint.GetInstance(new object());
+                Fail("GetInstance() failed to detect bad object.");
+            }
+            catch (ArgumentException)
+            {
+                // expected
+            }
+        }
 
-				Fail("GetInstance() failed to detect bad object.");
-			}
-			catch (ArgumentException)
-			{
-				// expected
-			}
-		}
+        private void CheckOnlyException(DistributionPointName distributionPoint, bool onlyContainsUserCerts,
+            bool onlyContainsCACerts, ReasonFlags onlySomeReasons, bool indirectCRL, bool onlyContainsAttributeCerts)
+        {
+            try
+            {
+                new IssuingDistributionPoint(distributionPoint, onlyContainsUserCerts, onlyContainsCACerts,
+                    onlySomeReasons, indirectCRL, onlyContainsAttributeCerts);
+                Fail("no exception");
+            }
+            catch (ArgumentException e)
+            {
+                IsEquals("only one of onlyContainsCACerts, onlyContainsUserCerts, or onlyContainsAttributeCerts can be true", e.Message);
+            }
+        }
 
-		private void checkPoint(
-			int						size,
-			DistributionPointName	distributionPoint,
-			bool					onlyContainsUserCerts,
-			bool					onlyContainsCACerts,
-			ReasonFlags				onlySomeReasons,
-			bool					indirectCRL,
-			bool					onlyContainsAttributeCerts)
-		{
-			IssuingDistributionPoint point = new IssuingDistributionPoint(distributionPoint, onlyContainsUserCerts, onlyContainsCACerts, onlySomeReasons, indirectCRL, onlyContainsAttributeCerts);
+        private void CheckPoint(int size, DistributionPointName distributionPoint, bool onlyContainsUserCerts,
+            bool onlyContainsCACerts, ReasonFlags onlySomeReasons, bool indirectCRL, bool onlyContainsAttributeCerts)
+        {
+            IssuingDistributionPoint point = new IssuingDistributionPoint(distributionPoint, onlyContainsUserCerts,
+                onlyContainsCACerts, onlySomeReasons, indirectCRL, onlyContainsAttributeCerts);
 
-			checkValues(point, distributionPoint, onlyContainsUserCerts, onlyContainsCACerts, onlySomeReasons, indirectCRL, onlyContainsAttributeCerts);
+            CheckValues(point, distributionPoint, onlyContainsUserCerts, onlyContainsCACerts, onlySomeReasons,
+                indirectCRL, onlyContainsAttributeCerts);
 
-			Asn1Sequence seq = Asn1Sequence.GetInstance(Asn1Object.FromByteArray(point.GetEncoded()));
+            Asn1Sequence seq = Asn1Sequence.GetInstance(point.GetEncoded());
+            IsEquals("size mismatch", seq.Count, size);
 
-			if (seq.Count != size)
-			{
-				Fail("size mismatch");
-			}
+            point = IssuingDistributionPoint.GetInstance(seq);
 
-			point = IssuingDistributionPoint.GetInstance(seq);
+            CheckValues(point, distributionPoint, onlyContainsUserCerts, onlyContainsCACerts, onlySomeReasons,
+                indirectCRL, onlyContainsAttributeCerts);
+        }
 
-			checkValues(point, distributionPoint, onlyContainsUserCerts, onlyContainsCACerts, onlySomeReasons, indirectCRL, onlyContainsAttributeCerts);
-		}
-
-		private void checkValues(
-			IssuingDistributionPoint	point,
-			DistributionPointName		distributionPoint,
-			bool						onlyContainsUserCerts,
-			bool						onlyContainsCACerts,
-			ReasonFlags					onlySomeReasons,
-			bool						indirectCRL,
-			bool						onlyContainsAttributeCerts)
-		{
-			if (point.OnlyContainsUserCerts != onlyContainsUserCerts)
-			{
-				Fail("mismatch on onlyContainsUserCerts");
-			}
-
-			if (point.OnlyContainsCACerts != onlyContainsCACerts)
-			{
-				Fail("mismatch on onlyContainsCACerts");
-			}
-
-			if (point.IsIndirectCrl != indirectCRL)
-			{
-				Fail("mismatch on indirectCRL");
-			}
-
-			if (point.OnlyContainsAttributeCerts != onlyContainsAttributeCerts)
-			{
-				Fail("mismatch on onlyContainsAttributeCerts");
-			}
-
-			if (!isEquiv(onlySomeReasons, point.OnlySomeReasons))
-			{
-				Fail("mismatch on onlySomeReasons");
-			}
-
-			if (!isEquiv(distributionPoint, point.DistributionPoint))
-			{
-				Fail("mismatch on distributionPoint");
-			}
-		}
-
-		private bool isEquiv(object o1, object o2)
-		{
-			if (o1 == null)
-			{
-				return o2 == null;
-			}
-
-			return o1.Equals(o2);
-		}
+        private void CheckValues(IssuingDistributionPoint point, DistributionPointName distributionPoint,
+            bool onlyContainsUserCerts, bool onlyContainsCACerts, ReasonFlags onlySomeReasons, bool indirectCRL,
+            bool onlyContainsAttributeCerts)
+        {
+            IsEquals("mismatch on onlyContainsUserCerts", point.OnlyContainsUserCerts, onlyContainsUserCerts);
+            IsEquals("mismatch on onlyContainsCACerts", point.OnlyContainsCACerts, onlyContainsCACerts);
+            IsEquals("mismatch on indirectCRL", point.IsIndirectCrl, indirectCRL);
+            IsEquals("mismatch on onlyContainsAttributeCerts", point.OnlyContainsAttributeCerts, onlyContainsAttributeCerts);
+            IsTrue("mismatch on onlySomeReasons", Object.Equals(onlySomeReasons, point.OnlySomeReasons));
+            IsTrue("mismatch on distributionPoint", Object.Equals(distributionPoint, point.DistributionPoint));
+        }
 
         [Test]
         public void TestFunction()
@@ -123,5 +90,5 @@ namespace Org.BouncyCastle.Asn1.Tests
 
             Assert.AreEqual(Name + ": Okay", resultText);
         }
-	}
+    }
 }
