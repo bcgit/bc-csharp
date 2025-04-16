539a76a8ef7ea55bbac89010552c59c9fb5262fc
Peter Dettman <peter.dettman@bouncycastle.org>
2025-04-16 16:03:55 +0700

    Example SLH-DSA credentials from draft-ietf-lamps-x509-slhdsa-04

---
+++ crypto/test/src/cert/test/SlhDsaCredentialsTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace Org.BouncyCastle.Cert.Tests
+{
+    [TestFixture]
+    public class SlhDsaCredentialsTest
+    {
+        [Test]
+        public void Sample_SLH_DSA_SHA2_128S()
+        {
+            CheckSampleCredentials(SampleCredentials.SLH_DSA_SHA2_128S);
+        }
+
+        private static void CheckSampleCredentials(SampleCredentials creds)
+        {
+            var cert = creds.Certificate;
+            cert.Verify(cert.GetPublicKey());
+        }
+    }
+}
