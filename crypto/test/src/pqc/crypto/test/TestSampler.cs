bb8ed0af371c59e176c11d044fb2e296732ed402
Peter Dettman <peter.dettman@bouncycastle.org>
2025-05-06 19:43:08 +0700

    Refactor TestSampler

--- crypto/test/src/pqc/crypto/test/TestSampler.cs
+++ crypto/test/src/pqc/crypto/test/TestSampler.cs
@@ -4,22 +4,22 @@ namespace Org.BouncyCastle.Pqc.Crypto.Tests
 {
     internal class TestSampler
     {
-        private readonly bool isFull;
-        private readonly int offSet;
+        private readonly bool m_full;
+        private readonly int m_offset;
 
         internal TestSampler()
         {
-            this.isFull = false;
-            this.offSet = new Random().Next() % 10;
+            // TODO: bc-java allows running exhaustive tests via property
+            //m_full = Properties.isOverrideSet("test.full");
+            m_full = false;
+
+            m_offset = new Random((int)(DateTime.UtcNow.Ticks / TimeSpan.TicksPerMillisecond)).Next(10);
         }
 
-        internal bool SkipTest(string count)
-        {
-            if (isFull)
-                return false;
+        internal bool SkipTest(string count) => !m_full && ShouldSkip(int.Parse(count));
 
-            int c = int.Parse(count);
-            return c != 0 && ((c + offSet) % 9 != 0);
-        }
+        internal bool SkipTest(int count) => !m_full && ShouldSkip(count);
+
+        private bool ShouldSkip(int count) => count != 0 && ((count + m_offset) % 9 != 0);
     }
 }
