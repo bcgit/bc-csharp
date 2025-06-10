4492223086ef84c11c7d411d4457665afbabecfc
Peter Dettman <peter.dettman@bouncycastle.org>
2025-06-10 16:44:21 +0700

    Update SrtpProtectionProfile

--- crypto/src/tls/SrtpProtectionProfile.cs
+++ crypto/src/tls/SrtpProtectionProfile.cs
@@ -1,7 +1,6 @@
-﻿using System;
-
-namespace Org.BouncyCastle.Tls
+﻿namespace Org.BouncyCastle.Tls
 {
+    // TODO[api] Make static
     public abstract class SrtpProtectionProfile
     {
         /*
@@ -9,6 +8,14 @@ namespace Org.BouncyCastle.Tls
          */
         public const int SRTP_AES128_CM_HMAC_SHA1_80 = 0x0001;
         public const int SRTP_AES128_CM_HMAC_SHA1_32 = 0x0002;
+
+        /// <summary>Removed by draft-ietf-avt-dtls-srtp-04.</summary>
+        /// <remarks>IANA: Unassigned.</remarks>
+        public const int DRAFT_SRTP_AES256_CM_SHA1_80 = 0x0003;
+        /// <summary>Removed by draft-ietf-avt-dtls-srtp-04.</summary>
+        /// <remarks>IANA: Unassigned.</remarks>
+        public const int DRAFT_SRTP_AES256_CM_SHA1_32 = 0x0004;
+
         public const int SRTP_NULL_HMAC_SHA1_80 = 0x0005;
         public const int SRTP_NULL_HMAC_SHA1_32 = 0x0006;
 
@@ -17,5 +24,21 @@ namespace Org.BouncyCastle.Tls
          */
         public const int SRTP_AEAD_AES_128_GCM = 0x0007;
         public const int SRTP_AEAD_AES_256_GCM = 0x0008;
+
+        /*
+         * RFC 8723 10.1.
+         */
+        public const int DOUBLE_AEAD_AES_128_GCM_AEAD_AES_128_GCM = 0x0009;
+        public const int DOUBLE_AEAD_AES_256_GCM_AEAD_AES_256_GCM = 0x000A;
+
+        /*
+         * RFC 8269 6.1.
+         */
+        public const int SRTP_ARIA_128_CTR_HMAC_SHA1_80 = 0x000B;
+        public const int SRTP_ARIA_128_CTR_HMAC_SHA1_32 = 0x000C;
+        public const int SRTP_ARIA_256_CTR_HMAC_SHA1_80 = 0x000D;
+        public const int SRTP_ARIA_256_CTR_HMAC_SHA1_32 = 0x000E;
+        public const int SRTP_AEAD_ARIA_128_GCM = 0x000F;
+        public const int SRTP_AEAD_ARIA_256_GCM = 0x0010;
     }
 }
