9c3b97d37b7af1ad2186a4221ecfdd4c97d526cf
Peter Dettman <peter.dettman@bouncycastle.org>
2025-06-30 19:08:00 +0700

    Refactor TlsCryptoParameters

--- crypto/src/tls/crypto/TlsCryptoParameters.cs
+++ crypto/src/tls/crypto/TlsCryptoParameters.cs
@@ -1,8 +1,6 @@
-﻿using System;
-
-namespace Org.BouncyCastle.Tls.Crypto
+﻿namespace Org.BouncyCastle.Tls.Crypto
 {
-    // TODO[tls-port] Would rather this be sealed
+    // TODO[api] Make sealed
     /// <summary>Carrier class for context-related parameters needed for creating secrets and ciphers.</summary>
     public class TlsCryptoParameters
     {
@@ -12,38 +10,20 @@ namespace Org.BouncyCastle.Tls.Crypto
         /// <param name="context">the context for this parameters object.</param>
         public TlsCryptoParameters(TlsContext context)
         {
-            this.m_context = context;
+            m_context = context;
         }
 
-        public SecurityParameters SecurityParameters
-        {
-            get { return m_context.SecurityParameters; }
-        }
+        public SecurityParameters SecurityParameters => m_context.SecurityParameters;
 
-        public ProtocolVersion ClientVersion
-        {
-            get { return m_context.ClientVersion; }
-        }
+        public ProtocolVersion ClientVersion => m_context.ClientVersion;
 
-        public ProtocolVersion RsaPreMasterSecretVersion
-        {
-            get { return m_context.RsaPreMasterSecretVersion; }
-        }
+        public ProtocolVersion RsaPreMasterSecretVersion => m_context.RsaPreMasterSecretVersion;
 
-        // TODO[tls-port] Would rather this be non-virtual
-        public virtual ProtocolVersion ServerVersion
-        {
-            get { return m_context.ServerVersion; }
-        }
+        // TODO[api] Make non-virtual
+        public virtual ProtocolVersion ServerVersion => m_context.ServerVersion;
 
-        public bool IsServer
-        {
-            get { return m_context.IsServer; }
-        }
+        public bool IsServer => m_context.IsServer;
 
-        public TlsNonceGenerator NonceGenerator
-        {
-            get { return m_context.NonceGenerator; }
-        }
+        public TlsNonceGenerator NonceGenerator => m_context.NonceGenerator;
     }
 }
