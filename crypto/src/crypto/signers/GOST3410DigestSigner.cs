3b4831401d473652b390c4438eead4e210e9fc50
Peter Dettman <peter.dettman@bouncycastle.org>
2025-07-16 17:39:37 +0700

    Refactor Gost3410DigestSigner

--- crypto/src/crypto/signers/GOST3410DigestSigner.cs
+++ crypto/src/crypto/signers/GOST3410DigestSigner.cs
@@ -1,90 +1,75 @@
 using System;
 
-using Org.BouncyCastle.Crypto.Parameters;
 using Org.BouncyCastle.Math;
 using Org.BouncyCastle.Security;
+using Org.BouncyCastle.Utilities;
 
 namespace Org.BouncyCastle.Crypto.Signers
 {
     public class Gost3410DigestSigner
         : ISigner
     {
-        private readonly IDigest digest;
-        private readonly IDsa dsaSigner;
-        private readonly int size;
-        private int halfSize;
-        private bool forSigning;
+        private readonly IDsa m_dsa;
+        private readonly IDigest m_digest;
+        private readonly int m_digestSize;
 
+        private bool m_forSigning;
+
+        // TODO[api] Rename 'signer' to 'dsa'
         public Gost3410DigestSigner(IDsa signer, IDigest digest)
         {
-            this.dsaSigner = signer;
-            this.digest = digest;
+            m_dsa = signer ?? throw new ArgumentNullException(nameof(signer));
+            m_digest = digest ?? throw new ArgumentNullException(nameof(digest));
 
-            halfSize = digest.GetDigestSize();
-            this.size = halfSize * 2;
-
+            m_digestSize = digest.GetDigestSize();
         }
 
-        public virtual string AlgorithmName
-        {
-            get { return digest.AlgorithmName + "with" + dsaSigner.AlgorithmName; }
-        }
+        public virtual string AlgorithmName => m_digest.AlgorithmName + "with" + m_dsa.AlgorithmName;
 
         public virtual void Init(bool forSigning, ICipherParameters parameters)
         {
-            this.forSigning = forSigning;
+            m_forSigning = forSigning;
 
-            AsymmetricKeyParameter k = (AsymmetricKeyParameter)ParameterUtilities.IgnoreRandom(parameters);
+            var key = (AsymmetricKeyParameter)ParameterUtilities.IgnoreRandom(parameters);
 
-            if (forSigning && !k.IsPrivate)
+            if (forSigning && !key.IsPrivate)
                 throw new InvalidKeyException("Signing requires private key.");
 
-            if (!forSigning && k.IsPrivate)
+            if (!forSigning && key.IsPrivate)
                 throw new InvalidKeyException("Verification requires public key.");
 
             Reset();
 
-            dsaSigner.Init(forSigning, parameters);
+            m_dsa.Init(forSigning, parameters);
         }
 
-        public virtual void Update(byte input)
-        {
-            digest.Update(input);
-        }
+        public virtual void Update(byte input) => m_digest.Update(input);
 
-        public virtual void BlockUpdate(byte[] input, int inOff, int inLen)
-        {
-            digest.BlockUpdate(input, inOff, inLen);
-        }
+        public virtual void BlockUpdate(byte[] input, int inOff, int inLen) =>
+            m_digest.BlockUpdate(input, inOff, inLen);
 
 #if NETCOREAPP2_1_OR_GREATER || NETSTANDARD2_1_OR_GREATER
-        public virtual void BlockUpdate(ReadOnlySpan<byte> input)
-        {
-            digest.BlockUpdate(input);
-        }
+        public virtual void BlockUpdate(ReadOnlySpan<byte> input) => m_digest.BlockUpdate(input);
 #endif
 
-        public virtual int GetMaxSignatureSize() => size;
+        public virtual int GetMaxSignatureSize() => m_digestSize * 2;
 
         public virtual byte[] GenerateSignature()
         {
-            if (!forSigning)
+            if (!m_forSigning)
                 throw new InvalidOperationException("GOST3410DigestSigner not initialised for signature generation.");
 
-            byte[] hash = new byte[digest.GetDigestSize()];
-            digest.DoFinal(hash, 0);
+            byte[] hash = DigestUtilities.DoFinal(m_digest);
 
             try
             {
-                BigInteger[] sig = dsaSigner.GenerateSignature(hash);
-                byte[] sigBytes = new byte[size];
+                BigInteger[] rs = m_dsa.GenerateSignature(hash);
 
-                // TODO Add methods to allow writing BigInteger to existing byte array?
-                byte[] r = sig[0].ToByteArrayUnsigned();
-                byte[] s = sig[1].ToByteArrayUnsigned();
-                s.CopyTo(sigBytes, halfSize - s.Length);
-                r.CopyTo(sigBytes, size - r.Length);
-                return sigBytes;
+                int halfSize = m_digestSize, size = halfSize * 2;
+                byte[] signature = new byte[size];
+                BigIntegers.AsUnsignedByteArray(rs[1], signature, 0, halfSize);
+                BigIntegers.AsUnsignedByteArray(rs[0], signature, halfSize, halfSize);
+                return signature;
             }
             catch (Exception e)
             {
@@ -94,29 +79,26 @@ namespace Org.BouncyCastle.Crypto.Signers
 
         public virtual bool VerifySignature(byte[] signature)
         {
-            if (forSigning)
+            if (m_forSigning)
                 throw new InvalidOperationException("GOST3410DigestSigner not initialised for verification");
 
-            byte[] hash = new byte[digest.GetDigestSize()];
-            digest.DoFinal(hash, 0);
+            byte[] hash = DigestUtilities.DoFinal(m_digest);
 
-            BigInteger R, S;
+            BigInteger r, s;
             try
             {
-                R = new BigInteger(1, signature, halfSize, halfSize);
-                S = new BigInteger(1, signature, 0, halfSize);
+                int halfSize = m_digestSize;
+                r = new BigInteger(1, signature, halfSize, halfSize);
+                s = new BigInteger(1, signature, 0, halfSize);
             }
             catch (Exception e)
             {
                 throw new SignatureException("error decoding signature bytes.", e);
             }
 
-            return dsaSigner.VerifySignature(hash, R, S);
+            return m_dsa.VerifySignature(hash, r, s);
         }
 
-        public virtual void Reset()
-        {
-            digest.Reset();
-        }
+        public virtual void Reset() => m_digest.Reset();
     }
 }
