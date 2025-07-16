44cabcb66c64397fd9f30142a65011d7b6ccc878
Peter Dettman <peter.dettman@bouncycastle.org>
2025-07-16 18:45:22 +0700

    Fix comments and whitespace

--- crypto/src/asn1/Asn1Encodable.cs
+++ crypto/src/asn1/Asn1Encodable.cs
@@ -2,8 +2,8 @@ using System.IO;
 
 namespace Org.BouncyCastle.Asn1
 {
-	public abstract class Asn1Encodable
-		: IAsn1Convertible
+    public abstract class Asn1Encodable
+        : IAsn1Convertible
     {
         public const string Ber = "BER";
         public const string Der = "DER";
@@ -13,51 +13,45 @@ namespace Org.BouncyCastle.Asn1
 
         public virtual void EncodeTo(Stream output, string encoding) => ToAsn1Object().EncodeTo(output, encoding);
 
-		// TODO[api] Make virtual and override in Asn1Object
-		public byte[] GetEncoded() => GetEncoded(Ber, preAlloc: 0, postAlloc: 0);
+        // TODO[api] Make virtual and override in Asn1Object
+        public byte[] GetEncoded() => GetEncoded(Ber, preAlloc: 0, postAlloc: 0);
 
         // TODO[api] Make virtual and override in Asn1Object
         public byte[] GetEncoded(string encoding) => GetEncoded(encoding, preAlloc: 0, postAlloc: 0);
 
         internal virtual byte[] GetEncoded(string encoding, int preAlloc, int postAlloc) =>
-			ToAsn1Object().GetEncoded(encoding, preAlloc, postAlloc);
+            ToAsn1Object().GetEncoded(encoding, preAlloc, postAlloc);
 
-        /**
-		* Return the DER encoding of the object, null if the DER encoding can not be made.
-		*
-		* @return a DER byte array, null otherwise.
-		*/
+        /// <summary>Return the DER encoding of the object, null if the DER encoding can not be made.</summary>
+        /// <returns>a DER byte array, null otherwise.</returns>
         public byte[] GetDerEncoded()
-		{
-			try
-			{
-				return GetEncoded(Der);
-			}
-			catch (IOException)
-			{
-				return null;
-			}
-		}
+        {
+            try
+            {
+                return GetEncoded(Der);
+            }
+            catch (IOException)
+            {
+                return null;
+            }
+        }
 
-		public sealed override int GetHashCode()
-		{
-			return ToAsn1Object().CallAsn1GetHashCode();
-		}
+        public sealed override int GetHashCode() => ToAsn1Object().CallAsn1GetHashCode();
 
-		public sealed override bool Equals(object obj)
-		{
-			if (obj == this)
-				return true;
+        public sealed override bool Equals(object obj)
+        {
+            if (obj == this)
+                return true;
 
-			if (!(obj is IAsn1Convertible that))
-				return false;
+            if (!(obj is IAsn1Convertible that))
+                return false;
 
-			Asn1Object o1 = this.ToAsn1Object();
-			Asn1Object o2 = that.ToAsn1Object();
+            Asn1Object o1 = this.ToAsn1Object();
+            Asn1Object o2 = that.ToAsn1Object();
 
-			return o1 == o2 || (null != o2 && o1.CallAsn1Equals(o2));
-		}
+            return o1 == o2 || (null != o2 && o1.CallAsn1Equals(o2));
+        }
 
-		public abstract Asn1Object ToAsn1Object();
+        public abstract Asn1Object ToAsn1Object();
     }
 }
