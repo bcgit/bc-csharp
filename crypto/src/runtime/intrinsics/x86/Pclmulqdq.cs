fa53d16e62352877dc112d3e08f72c2b61b9dde5
Peter Dettman <peter.dettman@bouncycastle.org>
2026-03-03 21:48:24 +0700

    Add Pclmulqdq.V256/V512

--- crypto/src/runtime/intrinsics/x86/Pclmulqdq.cs
+++ crypto/src/runtime/intrinsics/x86/Pclmulqdq.cs
@@ -8,6 +8,24 @@
         internal static bool IsEnabled => false;
 #endif
 
+        internal static class V256
+        {
+#if NET10_0_OR_GREATER
+            internal static bool IsEnabled => System.Runtime.Intrinsics.X86.Pclmulqdq.V256.IsSupported;
+#else
+            internal static bool IsEnabled => false;
+#endif
+        }
+
+        internal static class V512
+        {
+#if NET10_0_OR_GREATER
+            internal static bool IsEnabled => System.Runtime.Intrinsics.X86.Pclmulqdq.V512.IsSupported;
+#else
+            internal static bool IsEnabled => false;
+#endif
+        }
+
 //        internal static class X64
 //        {
 //#if NETCOREAPP3_0_OR_GREATER
