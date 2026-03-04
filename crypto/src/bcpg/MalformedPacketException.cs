62ec62929613a838e5438d3dfc633fdf48a63764
Peter Dettman <peter.dettman@bouncycastle.org>
2026-03-05 01:53:40 +0700

    Add MalformedPacketException

---
+++ crypto/src/bcpg/MalformedPacketException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Org.BouncyCastle.Bcpg
+{
+    [Serializable]
+    public class MalformedPacketException
+        : Exception
+    {
+        public MalformedPacketException()
+            : base()
+        {
+        }
+
+        public MalformedPacketException(string message)
+            : base(message)
+        {
+        }
+
+        public MalformedPacketException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        protected MalformedPacketException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
