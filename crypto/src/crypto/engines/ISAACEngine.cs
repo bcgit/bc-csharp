86d92a216f60777c6e66fe92f57ca6ced7bb4648
Peter Dettman <peter.dettman@bouncycastle.org>
2026-06-07 14:02:25 +0700

    Refactor IsaacEngine and tests

--- crypto/src/crypto/engines/ISAACEngine.cs
+++ crypto/src/crypto/engines/ISAACEngine.cs
@@ -6,228 +6,211 @@ using Org.BouncyCastle.Utilities;
 
 namespace Org.BouncyCastle.Crypto.Engines
 {
-    /**
-    * Implementation of Bob Jenkin's ISAAC (Indirection Shift Accumulate Add and Count).
-    * see: http://www.burtleburtle.net/bob/rand/isaacafa.html
-    */
+    /// <summary>Implementation of Bob Jenkin's ISAAC (Indirection Shift Accumulate Add and Count).</summary>
+    /// <remarks>
+    /// <see href="https://www.burtleburtle.net/bob/rand/isaacafa.html"/>
+    /// </remarks>
     public class IsaacEngine
         : IStreamCipher
     {
         // Constants
-        private static readonly int sizeL          = 8,
-                                    stateArraySize = sizeL<<5; // 256
+        private static readonly int SizeL = 8;
+        private static readonly int StateArraySize = SizeL << 5; // 256
 
         // Cipher's internal state
-        private uint[]   engineState   = null, // mm                
-                        results       = null; // randrsl
-        private uint     a = 0, b = 0, c = 0;
+        private readonly uint[] m_engineState = new uint[StateArraySize]; // mm
+        private uint m_a = 0, m_b = 0, m_c = 0;
 
         // Engine state
-        private int     index         = 0;
-        private byte[]  keyStream     = new byte[stateArraySize<<2], // results expanded into bytes
-                        workingKey    = null;
-        private bool	initialised   = false;
+        private readonly byte[] m_keyStream = new byte[StateArraySize << 2];
+        private int m_keyStreamPos = 0;
 
-        /**
-        * initialise an ISAAC cipher.
-        *
-        * @param forEncryption whether or not we are for encryption.
-        * @param params the parameters required to set up the cipher.
-        * @exception ArgumentException if the params argument is
-        * inappropriate.
-        */
-        public virtual void Init(
-            bool				forEncryption, 
-            ICipherParameters	parameters)
+        private byte[] m_workingKey = null;
+        private bool m_initialised = false;
+
+        public virtual string AlgorithmName => "ISAAC";
+
+        public virtual void Init(bool forEncryption, ICipherParameters parameters)
         {
-            if (!(parameters is KeyParameter))
-                throw new ArgumentException(
-                    "invalid parameter passed to ISAAC Init - " + Platform.GetTypeName(parameters),
-                    "parameters");
+            // ISAAC encryption/decryption is symmetrical so forEncryption is ignored
 
-            /* 
-            * ISAAC encryption and decryption is completely
-            * symmetrical, so the 'forEncryption' is 
-            * irrelevant.
-            */
-            KeyParameter p = (KeyParameter) parameters;
-            setKey(p.GetKey());
-        }
-
-        public virtual byte ReturnByte(
-            byte input)
-        {
-            if (index == 0) 
+            if (!(parameters is KeyParameter keyParameter))
             {
-                isaac();
-                keyStream = Pack.UInt32_To_BE(results);
+                var message = "invalid parameter passed to ISAAC Init - " + Platform.GetTypeName(parameters);
+                throw new ArgumentException(message, nameof(parameters));
             }
 
-            byte output = (byte)(keyStream[index]^input);
-            index = (index + 1) & 1023;
+            m_workingKey = keyParameter.GetKey();
+            m_initialised = true;
+
+            if (m_c > 1)
+            {
+                ClearKeyStream();
+            }
+
+            ResetCipher();
+        }
+
+        public virtual byte ReturnByte(byte input)
+        {
+            if (m_keyStreamPos == 0) 
+            {
+                Isaac();
+            }
+
+            byte output = (byte)(m_keyStream[m_keyStreamPos] ^ input);
+            m_keyStreamPos = (m_keyStreamPos + 1) & 1023;
 
             return output;
         }
 
-        public virtual void ProcessBytes(
-            byte[]	input, 
-            int		inOff, 
-            int		len, 
-            byte[]	output, 
-            int		outOff)
+        public virtual void ProcessBytes(byte[] input, int inOff, int len, byte[] output, int outOff)
         {
-            if (!initialised)
+#if NETCOREAPP2_1_OR_GREATER || NETSTANDARD2_1_OR_GREATER
+            ProcessBytes(input.AsSpan(inOff, len), output.AsSpan(outOff));
+#else
+            if (!m_initialised)
                 throw new InvalidOperationException(AlgorithmName + " not initialised");
 
             Check.DataLength(input, inOff, len, "input buffer too short");
             Check.OutputLength(output, outOff, len, "output buffer too short");
 
-            for (int i = 0; i < len; i++)
+            int pos = 0;
+            while (pos < len)
             {
-                if (index == 0) 
+                if (m_keyStreamPos == 0)
                 {
-                    isaac();
-                    keyStream = Pack.UInt32_To_BE(results);
+                    Isaac();
                 }
-                output[i+outOff] = (byte)(keyStream[index]^input[i+inOff]);
-                index = (index + 1) & 1023;
+
+                int xorLen = System.Math.Min(len - pos, m_keyStream.Length - m_keyStreamPos);
+                Bytes.Xor(xorLen, input, inOff + pos, m_keyStream, m_keyStreamPos, output, outOff + pos);
+
+                pos += xorLen;
+                m_keyStreamPos = (m_keyStreamPos + xorLen) & 1023;
             }
+#endif
         }
 
 #if NETCOREAPP2_1_OR_GREATER || NETSTANDARD2_1_OR_GREATER
         public virtual void ProcessBytes(ReadOnlySpan<byte> input, Span<byte> output)
         {
-            if (!initialised)
+            if (!m_initialised)
                 throw new InvalidOperationException(AlgorithmName + " not initialised");
 
             Check.OutputLength(output, input.Length, "output buffer too short");
 
-            for (int i = 0; i < input.Length; i++)
+            while (!input.IsEmpty)
             {
-                if (index == 0)
+                if (m_keyStreamPos == 0)
                 {
-                    isaac();
-                    keyStream = Pack.UInt32_To_BE(results);
+                    Isaac();
                 }
-                output[i] = (byte)(keyStream[index++] ^ input[i]);
-                index &= 1023;
+
+                int xorLen = System.Math.Min(input.Length, m_keyStream.Length - m_keyStreamPos);
+                Bytes.Xor(xorLen, input, m_keyStream.AsSpan(m_keyStreamPos), output);
+
+                input = input[xorLen..];
+                output = output[xorLen..];
+                m_keyStreamPos = (m_keyStreamPos + xorLen) & 1023;
             }
         }
 #endif
 
-        public virtual string AlgorithmName
-        {
-            get { return "ISAAC"; }
-        }
-
         public virtual void Reset()
         {
-            setKey(workingKey);
+            if (m_c > 1)
+            {
+                ClearKeyStream();
+                ResetCipher();
+            }
         }
 
-        // Private implementation
-        private void setKey(
-            byte[] keyBytes)
+        private void Isaac()
         {
-            workingKey = keyBytes;
+            uint a = m_a;
+            uint b = m_b + ++m_c;
 
-            if (engineState == null)
+            for (int i = 0; i < StateArraySize; i++)
             {
-                engineState = new uint[stateArraySize];
+                uint x = m_engineState[i];
+                switch (i & 3)
+                {
+                case 0: a ^= a << 13; break;
+                case 1: a ^= a >> 6; break;
+                case 2: a ^= a << 2; break;
+                case 3: a ^= a >> 16; break;
+                }
+                a += m_engineState[i ^ 0x80];
+                uint y = m_engineState[(x >> 2) & 0xFF] + a + b;
+                m_engineState[i] = y;
+                b = m_engineState[(y >> 10) & 0xFF] + x;
+                Pack.UInt32_To_BE(b, m_keyStream, i * 4);
             }
 
-            if (results == null)
+            m_a = a;
+            m_b = b;
+        }
+
+        private void ClearKeyStream()
+        {
+            Arrays.ZeroMemory(m_keyStream);
+            m_keyStreamPos = 0;
+        }
+
+        private void ResetCipher()
+        {
+            Arrays.Fill(m_engineState, 0U);
+            m_a = 0;
+            m_b = 0;
+            m_c = 0;
+
+            // Initialise the engine state from the little-endian key bytes
+            int fullKeyLength = m_workingKey.Length / 4, partialKeyLength = m_workingKey.Length & 3;
+            Pack.LE_To_UInt32(m_workingKey, 0, m_engineState, 0, fullKeyLength);
+            if (partialKeyLength != 0)
             {
-                results = new uint[stateArraySize];
-            }
-
-            int i, j, k;
-
-            // Reset state
-            for (i = 0; i < stateArraySize; i++)
-            {
-                engineState[i] = results[i] = 0;
-            }
-            a = b = c = 0;
-
-            // Reset index counter for output
-            index = 0;
-
-            // Convert the key bytes to ints and put them into results[] for initialization
-            byte[] t = new byte[keyBytes.Length + (keyBytes.Length & 3)];
-            Array.Copy(keyBytes, 0, t, 0, keyBytes.Length);
-            for (i = 0; i < t.Length; i+=4)
-            {
-                results[i >> 2] = Pack.LE_To_UInt32(t, i);
+                m_engineState[fullKeyLength] = Pack.LE_To_UInt32_Low(m_workingKey, fullKeyLength * 4, partialKeyLength);
             }
 
             // It has begun?
-            uint[] abcdefgh = new uint[sizeL];
+            uint[] abcdefgh = new uint[SizeL];
+            Arrays.Fill(abcdefgh, 0x9E3779B9U); // Phi (golden ratio)
 
-            for (i = 0; i < sizeL; i++)
+            for (int i = 0; i < 4; i++)
             {
-                abcdefgh[i] = 0x9e3779b9; // Phi (golden ratio)
+                Mix(abcdefgh);
             }
 
-            for (i = 0; i < 4; i++)
+            for (int i = 0; i < 2; i++)
             {
-                mix(abcdefgh);
-            }
-
-            for (i = 0; i < 2; i++)
-            {
-                for (j = 0; j < stateArraySize; j+=sizeL)
+                for (int j = 0; j < StateArraySize; j += SizeL)
                 {
-                    for (k = 0; k < sizeL; k++)
+                    for (int k = 0; k < SizeL; k++)
                     {
-                        abcdefgh[k] += (i<1) ? results[j+k] : engineState[j+k];
+                        abcdefgh[k] += m_engineState[j + k];
                     }
 
-                    mix(abcdefgh);
-
-                    for (k = 0; k < sizeL; k++)
-                    {
-                        engineState[j+k] = abcdefgh[k];
-                    }
+                    Mix(abcdefgh);
+                    Array.Copy(abcdefgh, 0, m_engineState, j, SizeL);
                 }
             }
 
-            isaac();
-
-            initialised = true;
-        }    
-
-        private void isaac()
-        {
-            uint x, y;
-
-            b += ++c;
-            for (int i = 0; i < stateArraySize; i++)
-            {
-                x = engineState[i];
-                switch (i & 3)
-                {
-                    case 0: a ^= (a << 13); break;
-                    case 1: a ^= (a >>  6); break;
-                    case 2: a ^= (a <<  2); break;
-                    case 3: a ^= (a >> 16); break;
-                }
-                a += engineState[(i+128) & 0xFF];
-                engineState[i] = y = engineState[(int)((uint)x >> 2) & 0xFF] + a + b;
-                results[i] = b = engineState[(int)((uint)y >> 10) & 0xFF] + x;
-            }
+            Isaac();
         }
 
-        private void mix(uint[] x)
+        private static void Mix(uint[] x)
         {
-            x[0]^=x[1]<< 11; x[3]+=x[0]; x[1]+=x[2];
-            x[1]^=x[2]>>  2; x[4]+=x[1]; x[2]+=x[3];
-            x[2]^=x[3]<<  8; x[5]+=x[2]; x[3]+=x[4];
-            x[3]^=x[4]>> 16; x[6]+=x[3]; x[4]+=x[5];
-            x[4]^=x[5]<< 10; x[7]+=x[4]; x[5]+=x[6];
-            x[5]^=x[6]>>  4; x[0]+=x[5]; x[6]+=x[7];
-            x[6]^=x[7]<<  8; x[1]+=x[6]; x[7]+=x[0];
-            x[7]^=x[0]>>  9; x[2]+=x[7]; x[0]+=x[1];
+            uint x0 = x[0], x1 = x[1], x2 = x[2], x3 = x[3], x4 = x[4], x5 = x[5], x6 = x[6], x7 = x[7];
+            x0 ^= x1 << 11; x3 += x0; x1 += x2;
+            x1 ^= x2 >>  2; x4 += x1; x2 += x3;
+            x2 ^= x3 <<  8; x5 += x2; x3 += x4;
+            x3 ^= x4 >> 16; x6 += x3; x4 += x5;
+            x4 ^= x5 << 10; x7 += x4; x5 += x6;
+            x5 ^= x6 >>  4; x0 += x5; x6 += x7;
+            x6 ^= x7 <<  8; x1 += x6; x7 += x0;
+            x7 ^= x0 >>  9; x2 += x7; x0 += x1;
+            x[0] = x0; x[1] = x1; x[2] = x2; x[3] = x3; x[4] = x4; x[5] = x5; x[6] = x6; x[7] = x7;
         }
     }
 }
