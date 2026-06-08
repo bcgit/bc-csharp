a17d8ff6da5ce668ba7f782fa5410acf7ac11476
Konradsop <konrad.baran@gmx.net>
2026-06-08 22:19:57 +0200

    docs: add XML documentation for PKCS#12 store API
    Document the in-memory PKCS#12 keystore surface (RFC 7292): Pkcs12Store,
    Pkcs12Entry and Pkcs12StoreBuilder. Adds class-level summaries and per-method
    <summary>, <param>, <returns>, and <exception> tags for Load, Save, alias
    management and builder configuration. Converts legacy block comment on
    GetCertificate to XML. No behavioural changes.

--- crypto/src/pkcs/PKCS12StoreBuilder.cs
+++ crypto/src/pkcs/PKCS12StoreBuilder.cs
@@ -3,6 +3,10 @@ using Org.BouncyCastle.Asn1.Pkcs;
 
 namespace Org.BouncyCastle.Pkcs
 {
+    /// <summary>
+    /// Fluent builder for <see cref="Pkcs12Store"/> instances, configuring PBE algorithms and
+    /// encoding options used when saving PKCS#12 files (RFC 7292).
+    /// </summary>
     // TODO[api] Make sealed
     public class Pkcs12StoreBuilder
     {
@@ -19,16 +23,26 @@ namespace Org.BouncyCastle.Pkcs
         private bool overwriteFriendlyName = true;
         private bool enableOracleTrustedKeyUsage = true;
 
+        /// <summary>Creates a builder with library-default PBE algorithms and options.</summary>
         public Pkcs12StoreBuilder()
         {
         }
 
+        /// <summary>
+        /// Builds a new <see cref="Pkcs12Store"/> using the configured algorithms and options.
+        /// </summary>
+        /// <returns>A new, empty PKCS#12 store.</returns>
         public Pkcs12Store Build()
         {
             return new Pkcs12Store(certAlgorithm, certPrfAlgorithm, keyAlgorithm, keyPrfAlgorithm, useDerEncoding,
                 reverseCertificates, overwriteFriendlyName, enableOracleTrustedKeyUsage);
         }
 
+        /// <summary>
+        /// Sets the PBE algorithm used to encrypt certificate bags when saving (PKCS#12 scheme 1).
+        /// </summary>
+        /// <param name="certAlgorithm">The certificate encryption algorithm OID.</param>
+        /// <returns>This builder instance.</returns>
         public Pkcs12StoreBuilder SetCertAlgorithm(DerObjectIdentifier certAlgorithm)
         {
             this.certAlgorithm = certAlgorithm;
@@ -36,8 +50,14 @@ namespace Org.BouncyCastle.Pkcs
             return this;
         }
 
-        // Specify a PKCS#5 Scheme 2 encryption for certs
-        public Pkcs12StoreBuilder SetCertAlgorithm(DerObjectIdentifier certAlgorithm, DerObjectIdentifier certPrfAlgorithm)
+        /// <summary>
+        /// Sets the PBES2 algorithm and PRF used to encrypt certificate bags when saving (PKCS#12 scheme 2).
+        /// </summary>
+        /// <param name="certAlgorithm">The certificate encryption algorithm OID.</param>
+        /// <param name="certPrfAlgorithm">The PRF algorithm OID.</param>
+        /// <returns>This builder instance.</returns>
+        public Pkcs12StoreBuilder SetCertAlgorithm(DerObjectIdentifier certAlgorithm,
+            DerObjectIdentifier certPrfAlgorithm)
         {
             this.certAlgorithm = certAlgorithm;
             this.certPrfAlgorithm = certPrfAlgorithm;
@@ -48,14 +68,19 @@ namespace Org.BouncyCastle.Pkcs
         /// Whether to include Oracle's TrustedKeyUsage attribute in CertBag attributes. Defaults to <c>true</c>.
         /// </summary>
         /// <remarks>The OID 2.16.840.1.113894.746875.1.1 is used for this attribute.</remarks>
-        /// <param name="enableOracleTrustedKeyUsage"></param>
-        /// <returns></returns>
+        /// <param name="enableOracleTrustedKeyUsage"><c>true</c> to emit the attribute when saving.</param>
+        /// <returns>This builder instance.</returns>
         public Pkcs12StoreBuilder SetEnableOracleTrustedKeyUsage(bool enableOracleTrustedKeyUsage)
         {
             this.enableOracleTrustedKeyUsage = enableOracleTrustedKeyUsage;
             return this;
         }
 
+        /// <summary>
+        /// Sets the PBE algorithm used to encrypt private-key bags when saving (PKCS#12 scheme 1).
+        /// </summary>
+        /// <param name="keyAlgorithm">The key encryption algorithm OID.</param>
+        /// <returns>This builder instance.</returns>
         public Pkcs12StoreBuilder SetKeyAlgorithm(DerObjectIdentifier keyAlgorithm)
         {
             this.keyAlgorithm = keyAlgorithm;
@@ -63,26 +88,48 @@ namespace Org.BouncyCastle.Pkcs
             return this;
         }
 
-        // Specify a PKCS#5 Scheme 2 encryption for keys
-        public Pkcs12StoreBuilder SetKeyAlgorithm(DerObjectIdentifier keyAlgorithm, DerObjectIdentifier keyPrfAlgorithm)
+        /// <summary>
+        /// Sets the PBES2 algorithm and PRF used to encrypt private-key bags when saving (PKCS#12 scheme 2).
+        /// </summary>
+        /// <param name="keyAlgorithm">The key encryption algorithm OID.</param>
+        /// <param name="keyPrfAlgorithm">The PRF algorithm OID.</param>
+        /// <returns>This builder instance.</returns>
+        public Pkcs12StoreBuilder SetKeyAlgorithm(DerObjectIdentifier keyAlgorithm,
+            DerObjectIdentifier keyPrfAlgorithm)
         {
             this.keyAlgorithm = keyAlgorithm;
             this.keyPrfAlgorithm = keyPrfAlgorithm;
             return this;
         }
 
+        /// <summary>
+        /// Controls whether <see cref="Pkcs12Store.SetFriendlyName"/> may replace an existing friendly name.
+        /// Defaults to <c>true</c>.
+        /// </summary>
+        /// <param name="overwriteFriendlyName"><c>true</c> to allow overwriting friendly names.</param>
+        /// <returns>This builder instance.</returns>
         public Pkcs12StoreBuilder SetOverwriteFriendlyName(bool overwriteFriendlyName)
         {
             this.overwriteFriendlyName = overwriteFriendlyName;
             return this;
         }
 
+        /// <summary>
+        /// When <c>true</c>, certificate and key bags are written in reverse insertion order when saving.
+        /// </summary>
+        /// <param name="reverseCertificates"><c>true</c> to reverse bag order on save.</param>
+        /// <returns>This builder instance.</returns>
         public Pkcs12StoreBuilder SetReverseCertificates(bool reverseCertificates)
         {
             this.reverseCertificates = reverseCertificates;
             return this;
         }
 
+        /// <summary>
+        /// When <c>true</c>, saved PKCS#12 structures use DER encoding instead of BER for inner content.
+        /// </summary>
+        /// <param name="useDerEncoding"><c>true</c> for definite-length DER encoding.</param>
+        /// <returns>This builder instance.</returns>
         public Pkcs12StoreBuilder SetUseDerEncoding(bool useDerEncoding)
         {
             this.useDerEncoding = useDerEncoding;
--- crypto/src/pkcs/Pkcs12Entry.cs
+++ crypto/src/pkcs/Pkcs12Entry.cs
@@ -6,6 +6,10 @@ using Org.BouncyCastle.Utilities.Collections;
 
 namespace Org.BouncyCastle.Pkcs
 {
+    /// <summary>
+    /// Base class for PKCS#12 bag entries (private keys and certificates) carrying optional
+    /// PKCS#9 bag attributes such as friendly name and local key identifier.
+    /// </summary>
     public abstract class Pkcs12Entry
     {
         private readonly IDictionary<DerObjectIdentifier, Asn1Encodable> m_attributes;
@@ -17,17 +21,35 @@ namespace Org.BouncyCastle.Pkcs
             m_attributes = attributes;
         }
 
+        /// <summary>
+        /// Gets the bag attribute value for the given object identifier, or <c>null</c> if absent.
+        /// </summary>
+        /// <param name="oid">The bag attribute OID.</param>
         public Asn1Encodable this[DerObjectIdentifier oid] => CollectionUtilities.GetValueOrNull(m_attributes, oid);
 
+        /// <summary>Gets the object identifiers of all bag attributes on this entry.</summary>
         public IEnumerable<DerObjectIdentifier> BagAttributeKeys => CollectionUtilities.Proxy(m_attributes.Keys);
 
+        /// <summary>
+        /// Returns <c>true</c> if this entry has a PKCS#9 friendly name attribute.
+        /// </summary>
         public bool HasFriendlyName => m_attributes.ContainsKey(PkcsObjectIdentifiers.Pkcs9AtFriendlyName);
 
+        /// <summary>
+        /// Sets or replaces the PKCS#9 friendly name bag attribute on this entry.
+        /// </summary>
+        /// <param name="friendlyName">The friendly name to store.</param>
         public void SetFriendlyName(string friendlyName)
         {
             m_attributes[PkcsObjectIdentifiers.Pkcs9AtFriendlyName] = new DerBmpString(friendlyName);
         }
 
+        /// <summary>
+        /// Attempts to retrieve a bag attribute by OID.
+        /// </summary>
+        /// <param name="oid">The bag attribute OID.</param>
+        /// <param name="attribute">The attribute value, if present.</param>
+        /// <returns><c>true</c> if the attribute was found.</returns>
         public bool TryGetAttribute(DerObjectIdentifier oid, out Asn1Encodable attribute) =>
             m_attributes.TryGetValue(oid, out attribute);
     }
