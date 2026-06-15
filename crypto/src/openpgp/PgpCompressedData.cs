0adb9de01ee0744e8c98a2cfc0eaa652fe76e36d
Peter Dettman <peter.dettman@bouncycastle.org>
2026-06-15 17:54:44 +0700

    Refactor PgpCompressedData

--- crypto/src/openpgp/PgpCompressedData.cs
+++ crypto/src/openpgp/PgpCompressedData.cs
@@ -4,45 +4,39 @@ using Org.BouncyCastle.Utilities.IO.Compression;
 
 namespace Org.BouncyCastle.Bcpg.OpenPgp
 {
-	/// <remarks>Compressed data objects</remarks>
+    /// <remarks>Compressed data objects</remarks>
     public class PgpCompressedData
-		: PgpObject
+        : PgpObject
     {
-        private readonly CompressedDataPacket data;
+        private readonly CompressedDataPacket m_data;
 
-		public PgpCompressedData(BcpgInputStream bcpgInput)
+        public PgpCompressedData(BcpgInputStream bcpgInput)
         {
             Packet packet = bcpgInput.ReadPacket();
             if (!(packet is CompressedDataPacket compressedDataPacket))
                 throw new IOException("unexpected packet in stream: " + packet);
 
-            this.data = compressedDataPacket;
+            m_data = compressedDataPacket;
         }
 
-		/// <summary>The algorithm used for compression</summary>
-        public CompressionAlgorithmTag Algorithm
-        {
-			get { return data.Algorithm; }
-        }
+        /// <summary>The algorithm used for compression</summary>
+        public CompressionAlgorithmTag Algorithm => m_data.Algorithm;
 
-		/// <summary>Get the raw input stream contained in the object.</summary>
-        public Stream GetInputStream()
-        {
-            return data.GetInputStream();
-        }
+        /// <summary>Get the raw input stream contained in the object.</summary>
+        public Stream GetInputStream() => m_data.GetInputStream();
 
-		/// <summary>Return an uncompressed input stream which allows reading of the compressed data.</summary>
+        /// <summary>Return an uncompressed input stream which allows reading of the compressed data.</summary>
         public Stream GetDataStream()
         {
             switch (Algorithm)
             {
-			case CompressionAlgorithmTag.Uncompressed:
-				return GetInputStream();
-			case CompressionAlgorithmTag.Zip:
+            case CompressionAlgorithmTag.Uncompressed:
+                return GetInputStream();
+            case CompressionAlgorithmTag.Zip:
                 return Zip.DecompressInput(GetInputStream());
             case CompressionAlgorithmTag.ZLib:
-				return ZLib.DecompressInput(GetInputStream());
-			case CompressionAlgorithmTag.BZip2:
+                return ZLib.DecompressInput(GetInputStream());
+            case CompressionAlgorithmTag.BZip2:
                 return Bzip2.DecompressInput(GetInputStream());
             default:
                 throw new PgpException("can't recognise compression algorithm: " + Algorithm);
