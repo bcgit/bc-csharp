a43dc01852614b0236f625a77b3e11153e74b670
Peter Dettman <peter.dettman@bouncycastle.org>
2026-06-17 18:56:56 +0700

    Cleanup X509ObjectIdentifiers

--- crypto/src/asn1/x509/X509ObjectIdentifiers.cs
+++ crypto/src/asn1/x509/X509ObjectIdentifiers.cs
@@ -23,27 +23,27 @@ namespace Org.BouncyCastle.Asn1.X509
         //   {iso(1) identified-organization(3) oiw(14) secsig(3) algorithms(2) 26 }    //
         public static readonly DerObjectIdentifier IdSha1 = new DerObjectIdentifier("1.3.14.3.2.26");
 
-		//
+        //
         // ripemd160 OBJECT IDENTIFIER ::=
         //      {iso(1) identified-organization(3) TeleTrust(36) algorithm(3) hashAlgorithm(2) RipeMD-160(1)}
         //
         public static readonly DerObjectIdentifier RipeMD160 = new DerObjectIdentifier("1.3.36.3.2.1");
 
-		//
+        //
         // ripemd160WithRSAEncryption OBJECT IDENTIFIER ::=
         //      {iso(1) identified-organization(3) TeleTrust(36) algorithm(3) signatureAlgorithm(3) rsaSignature(1) rsaSignatureWithripemd160(2) }
         //
         public static readonly DerObjectIdentifier RipeMD160WithRsaEncryption = new DerObjectIdentifier("1.3.36.3.3.1.2");
 
-		public static readonly DerObjectIdentifier IdEARsa = new DerObjectIdentifier("2.5.8.1.1");
+        public static readonly DerObjectIdentifier IdEARsa = new DerObjectIdentifier("2.5.8.1.1");
 
-		// id-pkix
-		public static readonly DerObjectIdentifier IdPkix = new DerObjectIdentifier("1.3.6.1.5.5.7");
+        // id-pkix
+        public static readonly DerObjectIdentifier IdPkix = new DerObjectIdentifier("1.3.6.1.5.5.7");
 
-		//
-		// private internet extensions
-		//
-		public static readonly DerObjectIdentifier IdPE = IdPkix.Branch("1");
+        //
+        // private internet extensions
+        //
+        public static readonly DerObjectIdentifier IdPE = IdPkix.Branch("1");
 
         /** 1.3.6.1.5.5.7.6 */
         public static readonly DerObjectIdentifier pkix_algorithms = IdPkix.Branch("6");
@@ -85,11 +85,11 @@ namespace Org.BouncyCastle.Asn1.X509
         public static readonly DerObjectIdentifier IdADOcsp = IdAD.Branch("1");
         public static readonly DerObjectIdentifier IdADCAIssuers = IdAD.Branch("2");
 
-		//
-		// OID for ocsp and crl uri in AuthorityInformationAccess extension
-		//
-		public static readonly DerObjectIdentifier OcspAccessMethod = IdADOcsp;
-		public static readonly DerObjectIdentifier CrlAccessMethod = IdADCAIssuers;
+        //
+        // OID for ocsp and crl uri in AuthorityInformationAccess extension
+        //
+        public static readonly DerObjectIdentifier OcspAccessMethod = IdADOcsp;
+        public static readonly DerObjectIdentifier CrlAccessMethod = IdADCAIssuers;
 
         public static readonly DerObjectIdentifier id_ce = new DerObjectIdentifier("2.5.29");
     }
