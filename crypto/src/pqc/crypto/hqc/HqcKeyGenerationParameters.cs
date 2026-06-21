c99827acad9528962f1521f1ec9bd20a46703ed2
Konradsop <konrad.baran@gmx.net>
2026-06-21 11:28:12 +0200

    docs: add XML documentation for HQC key parameters

--- crypto/src/pqc/crypto/hqc/HqcKeyGenerationParameters.cs
+++ crypto/src/pqc/crypto/hqc/HqcKeyGenerationParameters.cs
@@ -5,11 +5,16 @@ using Org.BouncyCastle.Security;
 
 namespace Org.BouncyCastle.Pqc.Crypto.Hqc
 {
+    /// <summary>Key generation parameters for HQC, binding a randomness source to an HQC parameter set.</summary>
     public class HqcKeyGenerationParameters
         : KeyGenerationParameters
     {
         private readonly HqcParameters m_parameters;
 
+        /// <summary>Creates key generation parameters for the given HQC parameter set.</summary>
+        /// <param name="random">The randomness source for key generation.</param>
+        /// <param name="param">The HQC parameter set to generate keys for.</param>
+        /// <exception cref="ArgumentNullException">If <paramref name="param"/> is null.</exception>
         // TODO[api] Rename to 'parameters'
         public HqcKeyGenerationParameters(SecureRandom random, HqcParameters param)
             : base(random, 256)
@@ -17,6 +22,7 @@ namespace Org.BouncyCastle.Pqc.Crypto.Hqc
             m_parameters = param ?? throw new ArgumentNullException(nameof(param));
         }
 
+        /// <summary>The HQC parameter set keys will be generated for.</summary>
         public HqcParameters Parameters => m_parameters;
     }
 }
