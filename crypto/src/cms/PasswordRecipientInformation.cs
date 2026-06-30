ba8fff3a31ea0374db76ffa3837855f2d05b2f1b
Peter Dettman <peter.dettman@bouncycastle.org>
2026-07-01 01:00:31 +0700

    Cleanup PasswordRecipientInformation

--- crypto/src/cms/PasswordRecipientInformation.cs
+++ crypto/src/cms/PasswordRecipientInformation.cs
@@ -1,5 +1,4 @@
 using System;
-using System.IO;
 
 using Org.BouncyCastle.Asn1;
 using Org.BouncyCastle.Asn1.Cms;
@@ -10,49 +9,35 @@ using Org.BouncyCastle.Security;
 
 namespace Org.BouncyCastle.Cms
 {
-	/**
-	 * the RecipientInfo class for a recipient who has been sent a message
-	 * encrypted using a password.
-	 */
-	public class PasswordRecipientInformation
-		: RecipientInformation
-	{
-		private readonly PasswordRecipientInfo	info;
+    /// <summary>The RecipientInfo class for a recipient who has been sent a message encrypted using a password.</summary>
+    public class PasswordRecipientInformation
+        : RecipientInformation
+    {
+        private readonly PasswordRecipientInfo m_info;
 
-		internal PasswordRecipientInformation(
-			PasswordRecipientInfo	info,
-			CmsSecureReadable		secureReadable)
-			: base(info.KeyEncryptionAlgorithm, secureReadable)
-		{
-			this.info = info;
-			this.rid = new RecipientID();
-		}
+        internal PasswordRecipientInformation(PasswordRecipientInfo info, CmsSecureReadable secureReadable)
+            : base(info.KeyEncryptionAlgorithm, secureReadable)
+        {
+            m_info = info ?? throw new ArgumentNullException(nameof(info));
+            this.rid = new RecipientID();
+        }
 
-		/**
-		 * return the object identifier for the key derivation algorithm, or null
-		 * if there is none present.
-		 *
-		 * @return OID for key derivation algorithm, if present.
-		 */
-		public virtual AlgorithmIdentifier KeyDerivationAlgorithm
-		{
-			get { return info.KeyDerivationAlgorithm; }
-		}
+        /// <summary>
+        /// Return the object identifier for the key derivation algorithm, or null if there is none present.
+        /// </summary>
+        public virtual AlgorithmIdentifier KeyDerivationAlgorithm => m_info.KeyDerivationAlgorithm;
 
-		/**
-		 * decrypt the content and return an input stream.
-		 */
-		public override CmsTypedStream GetContentStream(
-			ICipherParameters key)
-		{
-			try
-			{
-				AlgorithmIdentifier kekAlg = AlgorithmIdentifier.GetInstance(info.KeyEncryptionAlgorithm);
-				Asn1Sequence        kekAlgParams = (Asn1Sequence)kekAlg.Parameters;
-				byte[]              encryptedKey = info.EncryptedKey.GetOctets();
-				string              kekAlgName = DerObjectIdentifier.GetInstance(kekAlgParams[0]).Id;
-				string				cName = CmsEnvelopedHelper.GetRfc3211WrapperName(kekAlgName);
-				IWrapper			keyWrapper = WrapperUtilities.GetWrapper(cName);
+        /// <summary>Decrypt the content and return an input stream.</summary>
+        public override CmsTypedStream GetContentStream(ICipherParameters key)
+        {
+            try
+            {
+                AlgorithmIdentifier kekAlg = AlgorithmIdentifier.GetInstance(m_info.KeyEncryptionAlgorithm);
+                Asn1Sequence kekAlgParams = (Asn1Sequence)kekAlg.Parameters;
+                byte[] encryptedKey = m_info.EncryptedKey.GetOctets();
+                string kekAlgName = DerObjectIdentifier.GetInstance(kekAlgParams[0]).Id;
+                string cName = CmsEnvelopedHelper.GetRfc3211WrapperName(kekAlgName);
+                IWrapper keyWrapper = WrapperUtilities.GetWrapper(cName);
 
                 var iv = Asn1OctetString.GetInstance(kekAlgParams[1]);
 
@@ -61,24 +46,24 @@ namespace Org.BouncyCastle.Cms
 #if NETCOREAPP2_1_OR_GREATER || NETSTANDARD2_1_OR_GREATER
                 parameters = new ParametersWithIV(parameters, iv.GetOctetsSpan());
 #else
-				parameters = new ParametersWithIV(parameters, iv.GetOctets());
+                parameters = new ParametersWithIV(parameters, iv.GetOctets());
 #endif
 
                 keyWrapper.Init(false, parameters);
 
-				KeyParameter sKey = ParameterUtilities.CreateKeyParameter(
-					GetContentAlgorithmName(), keyWrapper.Unwrap(encryptedKey, 0, encryptedKey.Length));
+                KeyParameter sKey = ParameterUtilities.CreateKeyParameter(
+                    GetContentAlgorithmName(), keyWrapper.Unwrap(encryptedKey, 0, encryptedKey.Length));
 
-				return GetContentFromSessionKey(sKey);
-			}
-			catch (SecurityUtilityException e)
-			{
-				throw new CmsException("couldn't create cipher.", e);
-			}
-			catch (InvalidKeyException e)
-			{
-				throw new CmsException("key invalid in message.", e);
-			}
-		}
-	}
+                return GetContentFromSessionKey(sKey);
+            }
+            catch (SecurityUtilityException e)
+            {
+                throw new CmsException("couldn't create cipher.", e);
+            }
+            catch (InvalidKeyException e)
+            {
+                throw new CmsException("key invalid in message.", e);
+            }
+        }
+    }
 }
