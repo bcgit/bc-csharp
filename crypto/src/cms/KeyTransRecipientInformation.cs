fa82a3d6a1b97da1c148141ee86f155835d613a1
Peter Dettman <peter.dettman@bouncycastle.org>
2026-07-13 12:58:39 +0700

    Cleanup KeyTransRecipientInformation

--- crypto/src/cms/KeyTransRecipientInformation.cs
+++ crypto/src/cms/KeyTransRecipientInformation.cs
@@ -70,59 +70,48 @@ namespace Org.BouncyCastle.Cms
             return algOid.GetID();
         }
 
-		internal KeyParameter UnwrapKey(ICipherParameters key)
-		{
-			byte[] encryptedKey = m_info.EncryptedKey.GetOctets();
-
-			try
-			{
-				if (Asn1.Pkcs.PkcsObjectIdentifiers.IdRsaesOaep.Equals(keyEncAlg.Algorithm))
-				{
-					IKeyUnwrapper keyWrapper = new Asn1KeyUnwrapper(keyEncAlg.Algorithm, keyEncAlg.Parameters, key);
-
-					return ParameterUtilities.CreateKeyParameter(
-						GetContentAlgorithmName(), keyWrapper.Unwrap(encryptedKey, 0, encryptedKey.Length).Collect());
-				}
-				else
-				{
-					string keyExchangeAlgorithm = GetExchangeEncryptionAlgorithmName(keyEncAlg);
-					IWrapper keyWrapper = WrapperUtilities.GetWrapper(keyExchangeAlgorithm);
-					keyWrapper.Init(false, key);
-
-					// FIXME Support for MAC algorithm parameters similar to cipher parameters
-					return ParameterUtilities.CreateKeyParameter(
-						GetContentAlgorithmName(), keyWrapper.Unwrap(encryptedKey, 0, encryptedKey.Length));
-				}
-			}
-			catch (SecurityUtilityException e)
-			{
-				throw new CmsException("couldn't create cipher.", e);
-			}
-			catch (InvalidKeyException e)
-			{
-				throw new CmsException("key invalid in message.", e);
-			}
-//			catch (IllegalBlockSizeException e)
-			catch (DataLengthException e)
-			{
-				throw new CmsException("illegal blocksize in message.", e);
-			}
-//			catch (BadPaddingException e)
-			catch (InvalidCipherTextException e)
-			{
-				throw new CmsException("bad padding in message.", e);
-			}
-		}
-		
-		/**
-        * decrypt the content and return it as a byte array.
-        */
-        public override CmsTypedStream GetContentStream(
-            ICipherParameters key)
+        internal KeyParameter UnwrapKey(ICipherParameters key)
         {
-			KeyParameter sKey = UnwrapKey(key);
+            byte[] encryptedKey = m_info.EncryptedKey.GetOctets();
 
-			return GetContentFromSessionKey(sKey);
-		}
+            try
+            {
+                if (Asn1.Pkcs.PkcsObjectIdentifiers.IdRsaesOaep.Equals(keyEncAlg.Algorithm))
+                {
+                    IKeyUnwrapper keyWrapper = new Asn1KeyUnwrapper(keyEncAlg.Algorithm, keyEncAlg.Parameters, key);
+                    byte[] keyBytes = keyWrapper.Unwrap(encryptedKey, 0, encryptedKey.Length).Collect();
+                    return ParameterUtilities.CreateKeyParameter(GetContentAlgorithmName(), keyBytes);
+                }
+                else
+                {
+                    string keyExchangeAlgorithm = GetExchangeEncryptionAlgorithmName(keyEncAlg);
+                    IWrapper keyWrapper = WrapperUtilities.GetWrapper(keyExchangeAlgorithm);
+                    keyWrapper.Init(forWrapping: false, key);
+                    byte[] keyBytes = keyWrapper.Unwrap(encryptedKey, 0, encryptedKey.Length);
+
+                    // FIXME Support for MAC algorithm parameters similar to cipher parameters
+                    return ParameterUtilities.CreateKeyParameter(GetContentAlgorithmName(), keyBytes);
+                }
+            }
+            catch (SecurityUtilityException e)
+            {
+                throw new CmsException("couldn't create cipher.", e);
+            }
+            catch (InvalidKeyException e)
+            {
+                throw new CmsException("key invalid in message.", e);
+            }
+            catch (DataLengthException e)
+            {
+                throw new CmsException("illegal blocksize in message.", e);
+            }
+            catch (InvalidCipherTextException e)
+            {
+                throw new CmsException("bad padding in message.", e);
+            }
+        }
+
+        /// <summary>Decrypt the content and return it as a byte array.</summary>
+        public override CmsTypedStream GetContentStream(ICipherParameters key) => GetContentFromSessionKey(UnwrapKey(key));
     }
 }
