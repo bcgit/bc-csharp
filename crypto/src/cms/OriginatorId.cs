d74ce6d2a28b798b31dceb5117bf299b676ab814
Konradsop <konrad.baran@gmx.net>
2026-07-30 16:18:36 +0200

    Add XML documentation for CMS signer types

--- crypto/src/cms/OriginatorId.cs
+++ crypto/src/cms/OriginatorId.cs
@@ -5,10 +5,17 @@ using Org.BouncyCastle.X509.Store;
 
 namespace Org.BouncyCastle.Cms
 {
+    /// <summary>
+    /// Identifies a CMS originator by issuer and serial number or subject key identifier.
+    /// </summary>
     // TODO[api] sealed
     public class OriginatorID
         : X509CertStoreSelector, IEquatable<OriginatorID>
     {
+        /// <summary>Determines whether this identifier selects the same originator as <paramref name="other"/>.
+        /// </summary>
+        /// <param name="other">The identifier to compare.</param>
+        /// <returns><c>true</c> if the identifiers match; otherwise, <c>false</c>.</returns>
         public virtual bool Equals(OriginatorID other)
         {
             return other == null ? false
@@ -18,8 +25,10 @@ namespace Org.BouncyCastle.Cms
                 && MatchesIssuer(other);
         }
 
+        /// <inheritdoc/>
         public override bool Equals(object obj) => Equals(obj as OriginatorID);
 
+        /// <inheritdoc/>
         public override int GetHashCode()
         {
             return GetHashCodeOfSubjectKeyIdentifier()
