717b5cfc8db13e01945956f8db03229259690e4f
Peter Dettman <peter.dettman@bouncycastle.org>
2026-07-30 10:38:03 +0700

    ASN.1: Read BMPString content through DefiniteLengthInputStream.ToArray
    - avoid letting a small header drive a large eager allocation

--- crypto/src/asn1/DerBMPString.cs
+++ crypto/src/asn1/DerBMPString.cs
@@ -169,85 +169,12 @@ namespace Org.BouncyCastle.Asn1
 
         internal static DerBmpString CreatePrimitive(DefiniteLengthInputStream defIn)
         {
-            int remainingBytes = defIn.Remaining;
-            if (0 != (remainingBytes & 1))
+            if (0 != (defIn.Remaining & 1))
                 throw new IOException("malformed BMPString encoding encountered");
 
-            int length = remainingBytes / 2;
-
-#if NETCOREAPP2_1_OR_GREATER || NETSTANDARD2_1_OR_GREATER
-            return new DerBmpString(string.Create(length, defIn, (str, defIn) =>
-            {
-                int stringPos = 0;
-
-                Span<byte> buf = stackalloc byte[8];
-                while (remainingBytes >= 8)
-                {
-                    if (Streams.ReadFully(defIn, buf) != 8)
-                        throw new EndOfStreamException("EOF encountered in middle of BMPString");
-
-                    str[stringPos] = (char)BinaryPrimitives.ReadUInt16BigEndian(buf[0..]);
-                    str[stringPos + 1] = (char)BinaryPrimitives.ReadUInt16BigEndian(buf[2..]);
-                    str[stringPos + 2] = (char)BinaryPrimitives.ReadUInt16BigEndian(buf[4..]);
-                    str[stringPos + 3] = (char)BinaryPrimitives.ReadUInt16BigEndian(buf[6..]);
-                    stringPos += 4;
-                    remainingBytes -= 8;
-                }
-                if (remainingBytes > 0)
-                {
-                    if (Streams.ReadFully(defIn, buf) != remainingBytes)
-                        throw new EndOfStreamException("EOF encountered in middle of BMPString");
-
-                    int bufPos = 0;
-                    do
-                    {
-                        int b1 = buf[bufPos++] << 8;
-                        int b2 = buf[bufPos++] & 0xFF;
-                        str[stringPos++] = (char)(b1 | b2);
-                    }
-                    while (bufPos < remainingBytes);
-                }
-
-                if (0 != defIn.Remaining || str.Length != stringPos)
-                    throw new InvalidOperationException();
-            }));
-#else
-            char[] str = new char[length];
-            int stringPos = 0;
-
-            byte[] buf = new byte[8];
-            while (remainingBytes >= 8)
-            {
-                if (Streams.ReadFully(defIn, buf, 0, 8) != 8)
-                    throw new EndOfStreamException("EOF encountered in middle of BMPString");
-
-                str[stringPos    ] = (char)((buf[0] << 8) | (buf[1] & 0xFF));
-                str[stringPos + 1] = (char)((buf[2] << 8) | (buf[3] & 0xFF));
-                str[stringPos + 2] = (char)((buf[4] << 8) | (buf[5] & 0xFF));
-                str[stringPos + 3] = (char)((buf[6] << 8) | (buf[7] & 0xFF));
-                stringPos += 4;
-                remainingBytes -= 8;
-            }
-            if (remainingBytes > 0)
-            {
-                if (Streams.ReadFully(defIn, buf, 0, remainingBytes) != remainingBytes)
-                    throw new EndOfStreamException("EOF encountered in middle of BMPString");
-
-                int bufPos = 0;
-                do
-                {
-                    int b1 = buf[bufPos++] << 8;
-                    int b2 = buf[bufPos++] & 0xFF;
-                    str[stringPos++] = (char)(b1 | b2);
-                }
-                while (bufPos < remainingBytes);
-            }
-
-            if (0 != defIn.Remaining || str.Length != stringPos)
-                throw new InvalidOperationException();
-
-            return new DerBmpString(str);
-#endif
+            // NOTE: read through ToArray, which grows its buffer as bytes actually arrive, so a crafted (invalid) small
+            // header can't cause a large allocation (GBs) before any content is actually read.
+            return CreatePrimitive(defIn.ToArray());
         }
 
         private static DerBmpString CreatePrimitive(byte[] contents) => new DerBmpString(contents);
--- crypto/test/src/asn1/test/StringTest.cs
+++ crypto/test/src/asn1/test/StringTest.cs
@@ -1,98 +1,149 @@
+using System.IO;
 using System.Text;
 
 using NUnit.Framework;
 
+using Org.BouncyCastle.Utilities;
 using Org.BouncyCastle.Utilities.Test;
 
 namespace Org.BouncyCastle.Asn1.Tests
 {
-	/**
+    /**
 	* X.690 test example
 	*/
-	[TestFixture]
-	public class StringTest
-		: SimpleTest
-	{
-		public override string Name
-		{
-			get { return "String"; }
-		}
+    [TestFixture]
+    public class StringTest
+        : SimpleTest
+    {
+        public override string Name
+        {
+            get { return "String"; }
+        }
 
-		public override void PerformTest()
-		{
-			DerBitString bs = new DerBitString(
-				new byte[] { (byte)0x01,(byte)0x23,(byte)0x45,(byte)0x67,(byte)0x89,(byte)0xab,(byte)0xcd,(byte)0xef });
+        public override void PerformTest()
+        {
+            DerBitString bs = new DerBitString(
+                new byte[] { (byte)0x01, (byte)0x23, (byte)0x45, (byte)0x67, (byte)0x89, (byte)0xab, (byte)0xcd, (byte)0xef });
 
-			if (!bs.GetString().Equals("#0309000123456789ABCDEF"))
-			{
-				Fail("DerBitString.GetString() result incorrect");
-			}
+            if (!bs.GetString().Equals("#0309000123456789ABCDEF"))
+            {
+                Fail("DerBitString.GetString() result incorrect");
+            }
 
-			if (!bs.ToString().Equals("#0309000123456789ABCDEF"))
-			{
-				Fail("DerBitString.ToString() result incorrect");
-			}
+            if (!bs.ToString().Equals("#0309000123456789ABCDEF"))
+            {
+                Fail("DerBitString.ToString() result incorrect");
+            }
 
-			bs = new DerBitString(
-				new byte[] { (byte)0xfe,(byte)0xdc,(byte)0xba,(byte)0x98,(byte)0x76,(byte)0x54,(byte)0x32,(byte)0x10 });
+            bs = new DerBitString(
+                new byte[] { (byte)0xfe, (byte)0xdc, (byte)0xba, (byte)0x98, (byte)0x76, (byte)0x54, (byte)0x32, (byte)0x10 });
 
-			if (!bs.GetString().Equals("#030900FEDCBA9876543210"))
-			{
-				Fail("DerBitString.GetString() result incorrect");
-			}
+            if (!bs.GetString().Equals("#030900FEDCBA9876543210"))
+            {
+                Fail("DerBitString.GetString() result incorrect");
+            }
 
-			if (!bs.ToString().Equals("#030900FEDCBA9876543210"))
-			{
-				Fail("DerBitString.ToString() result incorrect");
-			}
+            if (!bs.ToString().Equals("#030900FEDCBA9876543210"))
+            {
+                Fail("DerBitString.ToString() result incorrect");
+            }
 
-			DerUniversalString us = new DerUniversalString(
-				new byte[] { (byte)0x01,(byte)0x23,(byte)0x45,(byte)0x67,(byte)0x89,(byte)0xab,(byte)0xcd,(byte)0xef });
+            DerUniversalString us = new DerUniversalString(
+                new byte[] { (byte)0x01, (byte)0x23, (byte)0x45, (byte)0x67, (byte)0x89, (byte)0xab, (byte)0xcd, (byte)0xef });
 
-			if (!us.GetString().Equals("#1C080123456789ABCDEF"))
-			{
-				Fail("DerUniversalString.GetString() result incorrect");
-			}
+            if (!us.GetString().Equals("#1C080123456789ABCDEF"))
+            {
+                Fail("DerUniversalString.GetString() result incorrect");
+            }
 
-			if (!us.ToString().Equals("#1C080123456789ABCDEF"))
-			{
-				Fail("DerUniversalString.ToString() result incorrect");
-			}
+            if (!us.ToString().Equals("#1C080123456789ABCDEF"))
+            {
+                Fail("DerUniversalString.ToString() result incorrect");
+            }
 
-			us = new DerUniversalString(
-				new byte[] { (byte)0xfe,(byte)0xdc,(byte)0xba,(byte)0x98,(byte)0x76,(byte)0x54,(byte)0x32,(byte)0x10 });
+            us = new DerUniversalString(
+                new byte[] { (byte)0xfe, (byte)0xdc, (byte)0xba, (byte)0x98, (byte)0x76, (byte)0x54, (byte)0x32, (byte)0x10 });
 
-			if (!us.GetString().Equals("#1C08FEDCBA9876543210"))
-			{
-				Fail("DerUniversalString.GetString() result incorrect");
-			}
+            if (!us.GetString().Equals("#1C08FEDCBA9876543210"))
+            {
+                Fail("DerUniversalString.GetString() result incorrect");
+            }
 
-			if (!us.ToString().Equals("#1C08FEDCBA9876543210"))
-			{
-				Fail("DerUniversalString.ToString() result incorrect");
-			}
+            if (!us.ToString().Equals("#1C08FEDCBA9876543210"))
+            {
+                Fail("DerUniversalString.ToString() result incorrect");
+            }
 
-			byte[] t61Bytes = new byte[] { 0xff, 0xfe, 0xfd, 0xfc, 0xfb, 0xfa, 0xf9, 0xf8 };
-			string t61String = Encoding.GetEncoding("iso-8859-1").GetString(t61Bytes, 0, t61Bytes.Length);
-			DerT61String t61 = new DerT61String(t61Bytes);
+            byte[] t61Bytes = new byte[] { 0xff, 0xfe, 0xfd, 0xfc, 0xfb, 0xfa, 0xf9, 0xf8 };
+            string t61String = Encoding.GetEncoding("iso-8859-1").GetString(t61Bytes, 0, t61Bytes.Length);
+            DerT61String t61 = new DerT61String(t61Bytes);
 
-			if (!t61.GetString().Equals(t61String))
-			{
-				Fail("DerT61String.GetString() result incorrect");
-			}
+            if (!t61.GetString().Equals(t61String))
+            {
+                Fail("DerT61String.GetString() result incorrect");
+            }
 
-			if (!t61.ToString().Equals(t61String))
-			{
-				Fail("DerT61String.ToString() result incorrect");
-			}
-		}
+            if (!t61.ToString().Equals(t61String))
+            {
+                Fail("DerT61String.ToString() result incorrect");
+            }
+        }
 
-		[Test]
-		public void TestFunction()
-		{
-			string resultText = Perform().ToString();
+        [Test]
+        public void TestFunction()
+        {
+            string resultText = Perform().ToString();
 
-			Assert.AreEqual(Name + ": Okay", resultText);
-		}
-	}
+            Assert.AreEqual(Name + ": Okay", resultText);
+        }
+
+        /// <summary>
+        /// A BMPString whose declared length is far larger than the data behind it must fail on the truncated content,
+        /// not on the way to allocating for the declared length.
+        /// </summary>
+        /// <remarks>
+        /// Reading through DefiniteLengthInputStream.ToArray grows the buffer as bytes arrive, so this should return
+        /// promptly with a truncation error.
+        /// </remarks>
+        [Test]
+        public void CheckBmpStringHostileLength()
+        {
+            //long maxMemory = Runtime.getRuntime().maxMemory();
+            //int limit = maxMemory > Integer.MAX_VALUE ? Integer.MAX_VALUE : (int)maxMemory;
+            int limit = Arrays.MaxLength;
+            int declared = (limit - 1024) & ~1;
+
+            byte[] header = new byte[]{ 0x1E, 0x84,
+                (byte)(declared >> 24), (byte)(declared >> 16), (byte)(declared >> 8), (byte)declared,
+                0x00, 0x41
+            };
+
+            try
+            {
+                // wrapped so the stream's size cannot be discovered - i.e. read off a socket, where
+                // the declared length is all the parser has to go on
+                using (var asn1 = new Asn1InputStream(new NoSeekMemoryStream(header)))
+                {
+                    asn1.ReadObject();
+                }
+
+                Assert.Fail("hostile BMPString length not rejected");
+            }
+            catch (IOException)
+            {
+                // expected: the content runs out long before the declared length
+            }
+        }
+
+        private class NoSeekMemoryStream
+            : MemoryStream
+        {
+            internal NoSeekMemoryStream(byte[] buffer)
+                : base(buffer, writable: false)
+            {
+            }
+
+            public override bool CanSeek => false;
+        }
+    }
 }
